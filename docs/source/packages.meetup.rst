d90f4aa8756dc3eef5e296962292bd132d56caf2
Joel Klinger <joel.klinger@nesta.org.uk>
2018-07-12 15:35:05 +0000

    finished country group testing, now doing group members

--- docs/source/packages.meetup.rst
+++ docs/source/packages.meetup.rst
@@ -1,7 +1,44 @@
-Meetup
-======
+.. include:: ../../packages/meetup/README.rst
+
+Country :math:`\rightarrow` Groups
+----------------------------------
+
+Start with a country (and Meetup category) and end up with Meetup groups.
 
 .. automodule:: packages.meetup.country_groups
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Groups :math:`\rightarrow` Members
+----------------------------------
+
+Start with Meetup groups and end up with Meetup members.
+
+.. automodule:: packages.meetup.groups_members
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Members :math:`\rightarrow` Groups
+----------------------------------
+
+Start with Meetup members and end up with Meetup groups.
+
+.. automodule:: packages.meetup.members_groups
+    :members:
+    :undoc-members:
+    :show-inheritance:
+
+
+Groups :math:`\rightarrow` Group details
+----------------------------------------
+
+Start with Meetup groups and end up with Meetup group details.
+
+.. automodule:: packages.meetup.group_details
     :members:
     :undoc-members:
     :show-inheritance:
--- packages/meetup/groups_members.py
+++ packages/meetup/groups_members.py
@@ -1,18 +1,11 @@
 import requests
 import logging
 import time
-# import ratelim
-
-# Local imports
-from utils.common.db_utils import read_all_results
-from utils.common.datapipeline import DataPipeline
 from retrying import retry
+import json
+import os
 
-# RATELIM_DUR = 50 * 60
-# RATELIM_QUERIES = 8500
 
-
-# @ratelim.patient(RATELIM_QUERIES, RATELIM_DUR)
 @retry(wait_random_min=2000, wait_random_max=60000, stop_max_attempt_number=10)
 def get_members(params):
     # Set the offset parameter and make the request
@@ -26,40 +19,42 @@ def get_members(params):
     return [row['id'] for row in data['results']]
 
 
-def run(config):
-    groups = read_all_results(config, 'input_db', 'input_table')
-    # Filter groups matching target country/category
-    groups = [row for row in groups
-              if row['country_name'] == config['parameters']['country']]
-    groups = [row for row in groups
-              if row['category_id'] == int(config['parameters']['category'])]
+def get_all_members(group_id, max_results):
+    member_ids = []
+    offset = 0
+    while True:
+        params = dict(offset=offset, page=max_results,
+                      key=os.environ["MEETUP_API_KEY"], group_id=group_id)
+        _results = get_members(params)
+        member_ids += _results
+        if len(_results) < max_results:
+            break
+        offset += 1
+
+    # Join together unique member ids
+    members = []
+    for member_id in set(member_ids):
+        row = dict(member_id=member_id,
+                   group_urlname=group_urlname,
+                   group_id=group_id)
+        members.append(row)
+    return members
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    
+    # Get input data from somewhere
+    with open("data/country_groups.json", "r") as f:
+        groups = json.load(f)
 
     # Collect group info
-    groups = set((row['id'], row['urlname'])
-                 for row in groups)
+    groups = set((row['id'], row['urlname']) for row in groups)
     logging.info("Got %s distinct groups from database", len(groups))
 
-    api_key = config["Meetup"]["api-key"]
-    max_results = 200
+    # Collect members
     output = []
     for group_id, group_urlname in groups:
-        member_ids = []
-        offset = 0
-        while True:
-            params = dict(offset=offset, page=max_results,
-                          key=api_key, group_id=group_id)
-            _results = get_members(params)
-            member_ids += _results
-            if len(_results) < max_results:
-                break
-            offset += 1
-        for member_id in set(member_ids):
-            row = dict(member_id=member_id,
-                       group_urlname=group_urlname,
-                       group_id=group_id)
-            output.append(row)
-
-    logging.info("Got %s rows of data", len(output))
-    with DataPipeline(config) as dp:
-        for row in output:
-            dp.insert(row)
+        members = get_all_members(group_id, max_results=200)
+        output += members
+    logging.info("Got %s members", len(output))
