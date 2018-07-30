3a66dc1eb4283cbcd64c2c818615e862496c9332
Joel Klinger <joel.klinger@nesta.org.uk>
2018-07-30 13:43:02 +0000

    Added travis hooks

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,7 +3,13 @@
 set -e
 set -x
 
-echo $PWD
+# Set paths, and check they work
+export WORLD_BORDERS=$HOME/nesta/packages/meetup/data/TM_WORLD_BORDERS_SIMPL-0.3.shp
+export LUIGI_CONFIG_PATH=$HOME/nesta/production/config/luigi.cfg
+ls $WORLD_BORDERS
+ls $LUIGI_CONFIG_PATH
+
+# Run every test
 for TOPDIRNAME in packages production tools;
 do
     TESTDIRS=$(find $TOPDIRNAME -name "test*" -type d)
