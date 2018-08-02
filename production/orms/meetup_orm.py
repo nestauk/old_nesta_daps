2258a12b07e7345659ebe6dcf78abc984c46c54e
Joel Klinger <joel.klinger@nesta.org.uk>
2018-08-02 13:46:41 +0000

    Added tests for the meetup ORM

--- production/orms/meetup_orm.py
+++ production/orms/meetup_orm.py
@@ -7,15 +7,16 @@ Meetup
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.mysql import VARCHAR, BIGINT, TEXT, DECIMAL
-from sqlalchemy.types import JSON, INT
+from sqlalchemy.types import JSON, INT, TIMESTAMP
 from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
-class Groups(Base):
+class Group(Base):
     __tablename__ = 'meetup_groups'
 
-    id = Column(BIGINT(20), primary_key=True)    
+    id = Column(BIGINT(20), primary_key=True, autoincrement=False)
     urlname = Column(VARCHAR(200), unique=True)
     category_name = Column(VARCHAR(50))
     category_shortname = Column(VARCHAR(50))
@@ -30,12 +31,23 @@ class Groups(Base):
     topics = Column(JSON)
     category_id = Column(INT)
     country_name = Column(VARCHAR(100))
-    SOMEKINDOFTIMESTAMP
+    timestamp = Column(TIMESTAMP)
 
-class GroupsMembers(Base):
+#    meetup_groups_members = relationship("GroupMember", 
+#                                         cascade="all", 
+#                                         backref="meetup_groups")
+
+
+class GroupMember(Base):
     __tablename__ = 'meetup_groups_members'
 
-    group_id = Column(BIGINT(20), ForeignKey("meetup_groups.id"), primary_key=True)
-    group_urlname = Column(VARCHAR(200), ForeignKey("meetup_groups.urlname"))
+    group_id = Column(BIGINT(20), 
+                      ForeignKey("meetup_groups.id"),
+                      primary_key=True)
+    group_urlname = Column(VARCHAR(200),                            
+                           ForeignKey("meetup_groups.urlname"))
     member_id = Column(BIGINT(20), primary_key=True)
+
+ #   meetup_groups = relationship("Group", 
+ #                                backref="meetup_groups_members")
 
