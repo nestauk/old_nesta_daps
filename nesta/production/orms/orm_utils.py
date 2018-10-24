ee4b431a96ce6b9da3a3c3cd7e85d796c0103d29
Russell Winch <russell.winch@nesta.org.uk>
2018-10-24 15:02:40 +0100

    Add helpers to create elasticsearch index and load configuration.

--- nesta/production/orms/orm_utils.py
+++ nesta/production/orms/orm_utils.py
@@ -4,6 +4,8 @@ from sqlalchemy.engine.url import URL
 import pymysql
 import os
 from sqlalchemy.exc import OperationalError
+from elasticsearch import Elasticsearch
+import json
 
 
 def get_class_by_tablename(Base, tablename):
@@ -67,4 +69,45 @@ def get_mysql_engine(db_env, section, database="production_tests"):
                   database=database)
     # Create the database
     return create_engine(url, connect_args={"charset":"utf8mb4"})
-    
+
+
+def get_elasticsearch_config(es_env, section):
+    '''Loads local configuration for elasticsearch.
+
+    Args:
+        es_env (str): name of the environmental variable holding the path to the config
+        section (str): section of the document holding the relevent configuration
+
+    Returns:
+        (dict): settings for elasticsearch
+    '''
+    conf_path = os.environ[es_env]
+    cp = ConfigParser()
+    cp.read(conf_path)
+    conf = dict(cp._sections[section])
+    es_config = {'host': conf['host'],
+                 'port': conf['port'],
+                 'index': conf['index'],
+                 'type': conf['type']
+                 }
+    return es_config
+
+
+def create_elasticsearch_index(index_name, es_client, config_path=None):
+    '''Wrapper for the elasticsearch library to create indices.
+
+    Args:
+        index_name (str): name of the new index
+        es_client (object): es client for the targer cluster
+        config_path (str): local path to the .json file containing the mapping and settings
+
+    Returns:
+        acknowledgement from the cluster
+    '''
+    config = None
+    if config_path:
+        with open(config_path) as f:
+            config = json.load(f)
+    response = es_client.indices.create(index=index_name, body=config)
+    print(response)
+    return response
