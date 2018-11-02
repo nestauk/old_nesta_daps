f62ebf89af9f762c8c06fccf1217998588c446d5
Russell Winch <russell.winch@nesta.org.uk>
2018-11-02 17:34:41 +0000

    Increase container memory for abstract mesh task.

--- nesta/production/routines/health_data/nih_data/nih_root_task.py
+++ nesta/production/routines/health_data/nih_data/nih_root_task.py
@@ -50,4 +50,5 @@ class RootTask(luigi.WrapperTask):
                   job_queue="HighPriority",
                   region_name="eu-west-2",
                   poll_time=10,
+                  memory=1024,
                   max_live_jobs=50)
