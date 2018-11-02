ff629dedf2b06cb91951ea234c20ba2abe0cfa82
Russell Winch <russell.winch@nesta.org.uk>
2018-11-02 17:20:04 +0000

    Removed test code from mesh batchable, added logging.

--- nesta/production/batchables/health_data/nih_abstract_mesh_data/run.py
+++ nesta/production/batchables/health_data/nih_abstract_mesh_data/run.py
@@ -1,5 +1,6 @@
 from ast import literal_eval
 from elasticsearch import Elasticsearch
+import logging
 import os
 from sqlalchemy.orm import sessionmaker
 
@@ -13,6 +14,8 @@ from nesta.production.orms.nih_orm import Abstracts
 
 
 def run():
+    logging.getLogger().setLevel(logging.INFO)
+
     bucket = os.environ["BATCHPAR_s3_bucket"]
     abstract_file = os.environ["BATCHPAR_s3_key"]
     dupe_file = os.environ["BATCHPAR_dupe_file"]
@@ -27,6 +30,7 @@ def run():
     # retrieve a batch of meshed terms
     mesh_terms = retrieve_mesh_terms(bucket, abstract_file)
     mesh_terms = format_mesh_terms(mesh_terms)
+    logging.info(f'retrieved {len(mesh_terms)} meshed abstracts')
 
     # retrieve duplicate map
     dupes = retrieve_duplicate_map(bucket, dupe_file)
@@ -40,13 +44,12 @@ def run():
                      'abstract_text': abstract.abstract_text
                      })
         duped_docs = dupes.get(doc_id)
-        if duped_docs:
-            for duped_doc in duped_docs:
-                docs.append({'doc_id': duped_doc,
-                             'mesh_terms': terms,
-                             'abstract_text': abstract.abstract_text
-                             })
-        break  # just testing
+        logging.info(f'Found duplicates: {duped_docs}')
+        for duped_doc in duped_docs:
+            docs.append({'doc_id': duped_doc,
+                         'mesh_terms': terms,
+                         'abstract_text': abstract.abstract_text
+                         })
 
     # apply schema
     docs = schema_transformer(docs, filename="nih.json",
@@ -55,12 +58,14 @@ def run():
 
     # output to elasticsearch
     es = Elasticsearch(es_config['internal_host'], port=es_config['port'], sniff_on_start=True)
-
+    logging.info(f'writing {len(docs)} documents to elasticsearch')
     for doc in docs:
         uid = doc.pop("doc_id")
         existing = es.get(es_config['index'], doc_type=es_config['type'], id=uid)['_source']
         doc = {**doc, **existing}
         es.index(es_config['index'], doc_type=es_config['type'], id=uid, body=doc)
+
+    logging.info('finished')
 
 
 if __name__ == '__main__':
