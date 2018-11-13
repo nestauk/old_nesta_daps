5e981253d35e82737a40353c0ef43fd1ac5df23e
Joel Klinger <joel.alexander.klinger@gmail.com>
2018-11-13 15:30:57 +0000

    Added ngrammer

---
+++ nesta/packages/nlp_utils/tests/test_ngrammer.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+from nesta.packages.nlp_utils.ngrammer import Ngrammer
+
+
+class TestNgrammer(TestCase):
+    def test_ngrammer(self):
+        ngrammer = Ngrammer()
+        ngrammer.ngrams.clear()
+        ngrammer.ngrams[3].add('convolutional_neural_networks')
+        ngrammer.ngrams[3].add('bed_and_breakfast')
+        ngrammer.ngrams[2].add('neural_networks')
+        document = ("This is a document about machine "
+                    "learning, convolutional neural networks, "
+                    "neural networks and bed and breakfast")
+        processed_doc = ngrammer.process_document(document)
+        for _, ngrams in ngrammer.ngrams.items():
+            for ng in ngrams:
+                self.assertIn(ng, processed_doc[0])
