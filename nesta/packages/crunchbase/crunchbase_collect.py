f372a6bf43f7f17747e9f220ecf09f6bb6540ba5
Russell Winch <russell.winch@nesta.org.uk>
2018-12-07 17:19:17 +0000

    Add packages to process crunchbase org data.

--- nesta/packages/crunchbase/crunchbase_collect.py
+++ nesta/packages/crunchbase/crunchbase_collect.py
@@ -1,11 +1,13 @@
 from contextlib import contextmanager
 import logging
+import pandas as pd
 import re
 import requests
 import tarfile
 from tempfile import NamedTemporaryFile
 
 from nesta.production.luigihacks import misctools
+from nesta.packages.geo_utils.country_iso_code import country_iso_code_to_name
 
 
 @contextmanager
@@ -29,7 +31,7 @@ def crunchbase_tar():
 
 
 def get_csvs():
-    """Gets a list of csv files withing the Crunchbase tar archive.
+    """Gets a list of csv files within the Crunchbase tar archive.
 
     Returns:
         list: all .csv files int the archive
@@ -43,6 +45,75 @@ def get_csvs():
         if tablename is not None:
             csvs.append(tablename.group(1))
     return csvs
+
+
+def rename_uuid_columns(data):
+    """Renames any columns called or containing `uuid`, to the convention of `id`.
+
+    Args:
+        data (:obj:`pandas.Dataframe`): dataframe with column names to amend
+
+    Returns:
+        (:obj:`pandas.Dataframe`): the original dataframe with amended column names
+    """
+    renames = {col: col.replace('uuid', 'id') for col in data}
+    return data.rename(columns=renames)
+
+
+def generate_composite_key(city=None, country=None):
+    """Generates a composite key to use as the primary key for the geographic data.
+
+    Args:
+        city (str): name of the city
+        country (str): name of the country
+
+    Returns:
+        (str): composite key
+    """
+    try:
+        city = city.replace(' ', '-').lower()
+        country = country.replace(' ', '-').lower()
+    except AttributeError:
+        raise ValueError(f"Invalid city or country name. City: {city} | Country: {country}")
+    return '_'.join([city, country])
+
+
+def process_orgs(orgs, cat_groups, org_descriptions):
+    """Processes the organizations data.
+
+    Args:
+        orgs (:obj:`pandas.Dataframe`): organizations data
+        cat_groups (:obj:`pandas.Dataframe`): category groups data
+        org_descriptions (:obj:`pandas.Dataframe`): long organization descriptions
+
+    Returns:
+        (:obj:`pandas.Dataframe`): processed organization data
+        (:obj:`pandas.Dataframe`): generated organization_category data
+    """
+    orgs['country'] = orgs['country_code'].apply(country_iso_code_to_name)
+    orgs = orgs.drop('country_code', axis=1)  # now redundant with country_alpha_3 appended
+
+    orgs['location_id'] = generate_composite_key(orgs.city, orgs.country)
+    # split categories_list and lookup the cat_groups table to generate the link table
+    # remove category_list and category_group_list columns
+    # process org descriptions and add to long_description column
+
+
+def get_files_from_tar(files):
+    """Converts csv files in the crunchbase tar into dataframes and returns them.
+
+    Args:
+        files (list): names of the files to extract (without .csv suffix)
+
+    Returns:
+        (:obj:`list` of :obj:`pandas.Dataframe`): the extracted files as dataframes
+    """
+    dfs = []
+    with crunchbase_tar() as tar:
+        for filename in files:
+            dfs.append(pd.read_csv(tar.extractfile(''.join([filename, '.csv'])),
+                                   low_memory=False))
+    return dfs
 
 
 if __name__ == '__main__':
--- nesta/packages/crunchbase/tests/test_crunchbase_collect.py
+++ nesta/packages/crunchbase/tests/test_crunchbase_collect.py
@@ -1,20 +1,37 @@
-from unittest import mock
+import pandas as pd
+from pandas.testing import assert_frame_equal
+import pytest
 
-from nesta.packages.crunchbase.crunchbase_collect import collect_file_from_s3
-
-BOTO3 = 'nesta.packages.crunchbase.crunchbase_collect.boto3'
-
-
-@mock.patch(BOTO3)
-def test_get_crunchbase_file_keys_calls_all_crunchbase_by_default(mocked_boto):
-    mocked_bucket = mock.Mock()
-    mocked_s3 = mock.Mock(return_value=mocked_bucket)
-    mocked_boto.resource.return_value = mock.Mock(return_value=mocked_s3)
+from nesta.packages.crunchbase.crunchbase_collect import rename_uuid_columns
+from nesta.packages.crunchbase.crunchbase_collect import generate_composite_key
 
 
-def test_get_crunchbase_file_keys_overrides_prefix():
-    pass
+def test_rename_uuid_columns():
+    test_df = pd.DataFrame({'uuid': [1, 2, 3],
+                            'org_uuid': [11, 22, 33],
+                            'other_id': [111, 222, 333]
+                            })
+
+    expected_result = pd.DataFrame({'id': [1, 2, 3],
+                                    'other_id': [111, 222, 333],
+                                    'org_id': [11, 22, 33]
+                                    })
+
+    assert_frame_equal(rename_uuid_columns(test_df), expected_result, check_like=True)
 
 
-def test_get_crunchbase_file_keys_overrides_bucket():
-    pass
+def test_generate_composite_key():
+    assert generate_composite_key('London', 'United Kingdom') == 'london_united-kingdom'
+    assert generate_composite_key('Paris', 'France') == 'paris_france'
+    assert generate_composite_key('Name-with hyphen', 'COUNTRY') == 'name-with-hyphen_country'
+
+
+def test_generate_composite_key_raises_error_with_invalid_input():
+    with pytest.raises(ValueError):
+        generate_composite_key(None, 'UK')
+
+    with pytest.raises(ValueError):
+        generate_composite_key('city_only')
+
+    with pytest.raises(ValueError):
+        generate_composite_key(1, 2)
