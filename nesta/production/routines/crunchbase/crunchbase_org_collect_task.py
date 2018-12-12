f3244679476479eeb9406dc4cb83da59e4dcf071
Russell Winch <russell.winch@nesta.org.uk>
2018-12-12 15:59:24 +0000

    Keep cat_groups as a df to allow processing of orgs.

--- nesta/production/routines/crunchbase/crunchbase_org_collect_task.py
+++ nesta/production/routines/crunchbase/crunchbase_org_collect_task.py
@@ -82,8 +82,8 @@ class OrgCollectTask(luigi.Task):
                                                                  'organization_descriptions'])
         # process category_groups
         cat_groups = rename_uuid_columns(cat_groups)
-        cat_groups = cat_groups.to_dict(orient='records')
-        self._insert_data(CategoryGroup, cat_groups)
+        cat_groups_rows = cat_groups.to_dict(orient='records')
+        self._insert_data(CategoryGroup, cat_groups_rows)
 
         # process organizations and categories
         processed_orgs, org_cats, missing_cat_groups = process_orgs(orgs, cat_groups, org_descriptions)
