bbd97ae84c21098402eaab0a4ce2e1964838ead1
George Richardson <georgerichardson@posteo.net>
2018-12-19 13:12:48 +0000

    add grouper

--- nesta/packages/misc_utils/misc.py
+++ nesta/packages/misc_utils/misc.py
@@ -1,6 +1,6 @@
 
 
-def chunks(l, n):
+def chunker(l, n):
     """ chunks
     Yield successive n-sized chunks from l.
     
@@ -10,3 +10,18 @@ def chunks(l, n):
     """
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
+def grouper(g, n, fill_value=None):
+    """ grouper
+    Yield successive n-sized groups from g.
+
+    Args:
+        g (generator or iter): a generator or iterable
+        n (int): size of groups to yield
+        fill_value: object to fill any hanging space in the last group
+
+    Returns:
+        (generator): generates groups of g with length n
+    """
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
