4d3327c14db80f75c28f56e61594968a581e780d
Russell Winch <russell.winch@nesta.org.uk>
2019-01-02 17:30:21 +0000

    Add luigi tasks for geocoding of non-org tables.

--- nesta/production/routines/crunchbase/crunchbase_root_task.py
+++ nesta/production/routines/crunchbase/crunchbase_root_task.py
@@ -41,7 +41,6 @@ class RootTask(luigi.WrapperTask):
                              country_col=Organization.country,
                              location_key_col=Organization.location_id,
                              insert_batch_size=self.insert_batch_size,
-                             # batchable=find_filepath_from_pathstub("batchables/crunchbase/crunchbase_collect"),
                              env_files=[find_filepath_from_pathstub("nesta/nesta/"),
                                         find_filepath_from_pathstub("config/mysqldb.config"),
                                         find_filepath_from_pathstub("config/crunchbase.config")],
@@ -51,4 +50,4 @@ class RootTask(luigi.WrapperTask):
                              region_name="eu-west-2",
                              poll_time=10,
                              memory=4096,
-                             max_live_jobs=20)
+                             max_live_jobs=2)
