f66a08fbcae2b60b62278a6f24aaa835541c0013
Russell Winch <russell.winch@nesta.org.uk>
2019-01-28 11:32:31 +0000

    Give schema transformer ability to consume a single dict.

--- nesta/packages/decorators/tests/test_schema_transform.py
+++ nesta/packages/decorators/tests/test_schema_transform.py
@@ -2,13 +2,15 @@ import pytest
 import mock
 import pandas as pd
 from nesta.packages.decorators.schema_transform import schema_transform
+from nesta.packages.decorators.schema_transform import schema_transformer
+
 
 class TestSchemaTransform():
 
     @staticmethod
     @pytest.fixture
     def test_data():
-        return [{"bad_col":1, "another_bad_col": 2, "one_more_bad_col": -1}]
+        return [{"bad_col": 1, "another_bad_col": 2, "one_more_bad_col": -1}]
 
     @staticmethod
     @pytest.fixture
@@ -27,7 +29,6 @@ class TestSchemaTransform():
         assert len(transformed.columns) == len(test_transformer)
         assert all(c in test_transformer.values()
                    for c in transformed.columns)
-
 
     @mock.patch('nesta.packages.decorators.schema_transform.load_transformer')
     def test_list_of_dict_transform(self, mocked_loader, test_transformer, test_data):
@@ -50,3 +51,12 @@ class TestSchemaTransform():
         with pytest.raises(ValueError) as e:
             wrapped()
         assert "Schema transform expects" in str(e.value)
+
+    @mock.patch('nesta.packages.decorators.schema_transform.load_transformer')
+    def test_single_dict(self, mocked_loader, test_transformer):
+        mocked_loader.return_value = test_transformer
+        test_data = {'bad_col': 111, 'another_bad_col': 222, 'stuff': 333}
+
+        transformed = schema_transformer(test_data, filename='dummy',
+                                         from_key='dummy', to_key='dummy')
+        assert transformed == {'good_col': 111, 'another_good_col': 222}
