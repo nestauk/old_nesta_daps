0e905312acf495d1496b6570893bfbb652310b79
Russell Winch <russell.winch@nesta.org.uk>
2019-01-28 15:04:14 +0000

    Add missing bucket luigi parameter.

--- nesta/production/routines/crunchbase/crunchbase_root_task.py
+++ nesta/production/routines/crunchbase/crunchbase_root_task.py
@@ -37,10 +37,11 @@ class RootTask(luigi.WrapperTask):
                                 test=not self.production,
                                 db_config_env="MYSQLDB",
                                 insert_batch_size=self.insert_batch_size,
+                                intermediate_bucket='nesta-production-intermediate'
                                 batchable=find_filepath_from_pathstub("batchables/crunchbase/crunchbase_elasticsearch"),
                                 env_files=[find_filepath_from_pathstub("nesta/nesta/"),
                                            find_filepath_from_pathstub("config/mysqldb.config")],
-                                           # find_filepath_from_pathstub("config/elastic.config")],
+                                           find_filepath_from_pathstub("config/elastic.config")],
                                 job_def="py36_amzn1_image",
                                 job_name=f"CrunchBaseElasticsearchTask-{_routine_id}",
                                 job_queue="HighPriority",
