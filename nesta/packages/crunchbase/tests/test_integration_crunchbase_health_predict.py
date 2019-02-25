4ec9088b3d3d9f0630e2c28101c609998a18ef95
Russell Winch <russell.winch@nesta.org.uk>
2019-02-25 12:41:11 +0000

    Add labeling process to integration test for crunchbase health.

--- nesta/packages/crunchbase/tests/test_integration_crunchbase_health_predict.py
+++ nesta/packages/crunchbase/tests/test_integration_crunchbase_health_predict.py
@@ -1,14 +1,32 @@
+import json
 import pandas as pd
-# import pytest
 
 from nesta.packages.crunchbase.predict.src.model import train
+from nesta.packages.crunchbase.crunchbase_collect import predict_health_flag
+
+
+# ******resolve this before merge...path needs to work on remote machines ********
+# TEST_PATH = 'nesta/packages/crunchbase/tests'
+TEST_PATH = 'tests'
+TRAINING_DATA = f'{TEST_PATH}/integration_test_training_data.csv'
+UNLABELED_DATA = f'{TEST_PATH}/integration_test_dataset_to_label.json'
+EXPECTED_LABELED_DATA = f'{TEST_PATH}/integration_test_expected_labeled_dataset.json'
 
 
 def test_train_and_predict():
-    train_data = pd.read_csv('integration_test_training_data.csv')
+    train_data = pd.read_csv(TRAINING_DATA)
     assert train_data.shape == (5000, 2), 'Training data not shaped as expected'
 
+    # train and check accuracy
     vec, gs, accuracy = train(train_data)
     assert accuracy == 0.997
 
+    # collect unlabeled and expected data
+    with open(UNLABELED_DATA) as f:
+        unlabeled_data = json.load(f)
+    with open(EXPECTED_LABELED_DATA) as f:
+        expected_data = json.load(f)
 
+    # predict and check labels
+    labeled_data = predict_health_flag(unlabeled_data, vec, gs)
+    assert labeled_data == expected_data
