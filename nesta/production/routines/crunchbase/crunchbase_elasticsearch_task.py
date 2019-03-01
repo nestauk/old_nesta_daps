0a57da8463c62d09b4e6c4b26b21fcf0db0b225e
Russell Winch <russell.winch@nesta.org.uk>
2019-03-01 10:43:43 +0000

    Add missing Luigi parameter.

--- nesta/production/routines/crunchbase/crunchbase_elasticsearch_task.py
+++ nesta/production/routines/crunchbase/crunchbase_elasticsearch_task.py
@@ -53,6 +53,7 @@ class ElasticsearchTask(autobatch.AutoBatchTask):
                                   _routine_id=self._routine_id,
                                   test=self.test,
                                   insert_batch_size=self.insert_batch_size,
+                                  db_config_path=self.db_config_path,
                                   db_config_env=self.db_config_env)
 
     def output(self):
