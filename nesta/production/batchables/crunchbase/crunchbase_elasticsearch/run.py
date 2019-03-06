855a82e1f87e81602f1925cc385bf4f50d8e8d1d
Russell Winch <russell.winch@nesta.org.uk>
2019-03-06 07:19:47 +0000

    Add mesh terms to elastisearch task, plus a bit of tidying and refactoring.

--- nesta/production/batchables/crunchbase/crunchbase_elasticsearch/run.py
+++ nesta/production/batchables/crunchbase/crunchbase_elasticsearch/run.py
@@ -25,61 +25,63 @@ def run():
     # database setup
     engine = get_mysql_engine("BATCHPAR_config", "mysqldb", db_name)
 
-    # s3 setup
+    # elasticsearch setup
     es = Elasticsearch(es_host, port=es_port)
 
-    # collect file
-    nrows = 1000 if test else None
-
+    # retrieve batch org_ids from s3
     s3 = boto3.resource('s3')
     obj = s3.Object(bucket, batch_file)
     org_ids = json.loads(obj.get()['Body']._raw_stream.read())
     logging.info(f"{len(org_ids)} organisations retrieved from s3")
 
-    geo_fields = ['country_alpha_2', 'country_alpha_3', 'country_numeric', 'continent', 'latitude', 'longitude']
+    geo_fields = ['country_alpha_2', 'country_alpha_3', 'country_numeric',
+                  'continent', 'latitude', 'longitude']
+    nrows = 1000 if test else None
+
+    # collect orgs and geo data from database
     with db_session(engine) as session:
         rows = (session
                 .query(Organization, Geographic)
-                .join(Geographic, Organization.location_id==Geographic.id)
+                .join(Geographic, Organization.location_id == Geographic.id)
                 .filter(Organization.id.in_(org_ids))
                 .limit(nrows)
                 .all())
         for count, row in enumerate(rows, 1):
-            # convert sqlalchemy to dictionary
+            # convert sqlalchemy to dict
             row_combined = {k: v for k, v in row.Organization.__dict__.items()}
-            row_combined['currency_of_funding'] = 'USD'  # all values are from 'funding_total_usd'
+            row_combined.update({k: v for k, v in row.Geographic.__dict__.items()
+                                 if k in geo_fields})
 
-            row_combined.update({k: v for k, v in row.Geographic.__dict__.items() if k in geo_fields})
-
-            # reformat coordinates
+            # reformatting
             row_combined['coordinates'] = {'lat': row_combined.pop('latitude'),
                                            'lon': row_combined.pop('longitude')}
+            row_combined['mesh_terms'] = row_combined['mesh_terms'].split('|')
+            row_combined['currency_of_funding'] = 'USD'  # all from 'funding_total_usd'
 
-            # iterate through categories and groups
+            # extract categories and category groups
             row_combined['category_list'] = []
             row_combined['category_group_list'] = []
-            for category in (session.query(CategoryGroup)
+            for category in (session
+                             .query(CategoryGroup)
                              .select_from(OrganizationCategory)
                              .join(CategoryGroup)
-                             .filter(OrganizationCategory.organization_id==row.Organization.id)
+                             .filter(OrganizationCategory.organization_id == row.Organization.id)
                              .all()):
                 row_combined['category_list'].append(category.category_name)
-                row_combined['category_group_list'] += [group for group
-                                                        in str(category.category_group_list).split('|')
-                                                        if group is not 'None']
+                if category.category_group_list is not None:
+                    row_combined['category_group_list'] += category.category_group_list.split('|')
 
+            # schema transform and write to elasticsearch
+            uid = row_combined.pop('id')
             row_combined = schema_transformer(row_combined,
                                               filename='crunchbase_organisation_members.json',
                                               from_key='tier_0',
-                                              to_key='tier_1',
-                                              ignore=['id'])
-            uid = row_combined.pop('id')
+                                              to_key='tier_1')
             es.index(es_index, doc_type=es_type, id=uid, body=row_combined)
-
             if not count % 1000:
                 logging.info(f"{count} rows loaded to elasticsearch")
 
-    logging.warning("Batch job complete.")
+    logging.warning("Batch job complete")
 
 
 if __name__ == "__main__":
