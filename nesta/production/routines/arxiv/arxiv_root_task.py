9eb61790f2540966f278917caecb6774db0a0679
Russell Winch <russell.winch@nesta.org.uk>
2019-03-13 08:22:47 +0000

    Rename arxiv batch collection task.

--- nesta/production/routines/arxiv/arxiv_root_task.py
+++ nesta/production/routines/arxiv/arxiv_root_task.py
@@ -10,7 +10,7 @@ import datetime
 import logging
 from nesta.production.luigihacks.misctools import find_filepath_from_pathstub
 
-from arxiv_collect_task import CollectTask
+from arxiv_collect_task import CollectAllTask
 
 
 class RootTask(luigi.WrapperTask):
@@ -33,19 +33,19 @@ class RootTask(luigi.WrapperTask):
         _routine_id = "{}-{}".format(self.date, self.production)
 
         logging.getLogger().setLevel(logging.INFO)
-        yield CollectTask(date=self.date,
-                          _routine_id=_routine_id,
-                          db_config_path=self.db_config_path,
-                          test=(not self.production),
-                          batchable=find_filepath_from_pathstub("batchables/arxiv/arxiv_collect"),
-                          env_files=[find_filepath_from_pathstub("nesta/nesta/"),
-                                     find_filepath_from_pathstub("config/mysqldb.config"),
-                                     ],
-                          job_def="py36_amzn1_image",
-                          job_name="ArxivCollectTask-%s" % _routine_id,
-                          job_queue="MinimalCpus",
-                          region_name="eu-west-2",
-                          vcpus=2,
-                          poll_time=10,
-                          memory=2048,
-                          max_live_jobs=10)
+        yield CollectAllTask(date=self.date,
+                             _routine_id=_routine_id,
+                             db_config_path=self.db_config_path,
+                             test=(not self.production),
+                             batchable=find_filepath_from_pathstub("batchables/arxiv/arxiv_collect"),
+                             env_files=[find_filepath_from_pathstub("nesta/nesta/"),
+                                        find_filepath_from_pathstub("config/mysqldb.config"),
+                                        ],
+                             job_def="py36_amzn1_image",
+                             job_name="ArxivCollectAllTask-%s" % _routine_id,
+                             job_queue="MinimalCpus",
+                             region_name="eu-west-2",
+                             vcpus=2,
+                             poll_time=10,
+                             memory=2048,
+                             max_live_jobs=10)
