44e9d18825a4e44914981ce960be5561caee859b
Joel Klinger <joel.klinger@nesta.org.uk>
2019-05-22 09:55:14 +0000

    fixed locale issue by setting encoding on the docker build

---
+++ nesta/production/orms/tests/test_crunchbase.py
@@ -0,0 +1,24 @@
+import unittest
+from sqlalchemy.orm import sessionmaker
+from nesta.production.orms.crunchbase_orm import Base
+from nesta.production.orms.orm_utils import get_mysql_engine
+
+
+class TestCrunchbase(unittest.TestCase):
+    '''Check that the Crunchbase ORM works as expected'''
+    engine = get_mysql_engine("MYSQLDBCONF", "mysqldb")
+    Session = sessionmaker(engine)
+
+    def setUp(self):
+        '''Create the temporary table'''
+        Base.metadata.create_all(self.engine)
+
+    def tearDown(self):
+        '''Drop the temporary table'''
+        Base.metadata.drop_all(self.engine)
+
+    def test_build(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
