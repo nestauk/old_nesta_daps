d040385d39298a9b7828c963985a711fd79dd5ef
Joel Klinger <joel.klinger@nesta.org.uk>
2019-06-05 19:56:05 +0000

    Geocoding implemented for meetup data

--- nesta/production/luigihacks/batchgeocode.py
+++ nesta/production/luigihacks/batchgeocode.py
@@ -4,12 +4,16 @@ import logging
 import luigi
 from sqlalchemy import collate
 import time
+import os
 
+from nesta.packages.geo_utils.geocode import generate_composite_key
+from nesta.packages.geo_utils.country_iso_code import country_iso_code_to_name
 from nesta.production.luigihacks.autobatch import AutoBatchTask
 from nesta.production.luigihacks.misctools import find_filepath_from_pathstub
 from nesta.production.orms.geographic_orm import Base, Geographic
 from nesta.production.orms.orm_utils import get_mysql_engine, try_until_allowed, insert_data, db_session
-
+from nesta.production.luigihacks.misctools import get_config
+from nesta.production.luigihacks.mysqldb import MySqlTarget
 
 class GeocodeBatchTask(AutoBatchTask):
     """Appends various geographic codes to the geographic_data table using the
@@ -29,13 +33,23 @@ class GeocodeBatchTask(AutoBatchTask):
         batchable (str): location of the batchable run.py
     """
     test = luigi.BoolParameter()
+    _routine_id = luigi.Parameter(default="DUMMY ROUTINE")
     db_config_env = luigi.Parameter()
     city_col = luigi.Parameter()
     country_col = luigi.Parameter()
+    country_is_iso2 = luigi.BoolParameter(default=False)
     location_key_col = luigi.Parameter(default=None)
     batch_size = luigi.IntParameter(default=1000)
     intermediate_bucket = luigi.Parameter(default="nesta-production-intermediate")
     batchable = luigi.Parameter(default=find_filepath_from_pathstub("batchables/batchgeocode"))
+
+    def output(self):
+        '''Points to the output database engine'''
+        db_config = get_config(os.environ[self.db_config_env], "mysqldb")
+        db_config["database"] = 'dev' if self.test else 'production'
+        db_config["table"] = f"BatchGeocode{self._routine_id} <dummy>"  # Note, not a real table
+        return MySqlTarget(update_id=f"BatchGeocode-{self._routine_id}", **db_config)
+
 
     def _insert_new_locations(self):
         """Checks for new city/country combinations and appends them to the geographic
@@ -65,19 +79,28 @@ class GeocodeBatchTask(AutoBatchTask):
         """Checks for new city/country combinations and appends them to the geographic
         data table in mysql IF NO location_key_col IS PROVIDED.
         """
-        limit = 100 if self.test else None
+        limit = 100 if self.test else None        
         with db_session(self.engine) as session:
             existing_location_ids = {i[0] for i in session.query(Geographic.id).all()}
             new_locations = []
             all_locations = {(city, country) for city, country in
                              (session.query(self.city_col, self.country_col).limit(limit))}
+            nulls = 0
             for city, country in all_locations:
+                if self.country_is_iso2:
+                    country = country_iso_code_to_name(country, iso2=True)
+                if city is None or country is None:
+                    nulls += 1
+                    continue
                 key = generate_composite_key(city, country)
                 if key not in existing_location_ids and key is not None:
                     logging.info(f"new location {city}, {country}")
                     new_locations.append(dict(id=key, city=city, country=country))
                     existing_location_ids.add(key)
 
+        if nulls > 0:
+            logging.warning(f"{nulls} locations had a null city or "
+                            "country, so won't be processed.")
         if new_locations:
             logging.warning(f"Adding {len(new_locations)} new locations to database")
             insert_data(self.db_config_env, "mysqldb", self.database,
@@ -177,13 +200,16 @@ class GeocodeBatchTask(AutoBatchTask):
 
         return job_params
 
+    def combine(self, job_params):
+        '''Touch the checkpoint'''
+        self.output().touch()
+
 
 if __name__ == '__main__':
     from nesta.production.orms.crunchbase_orm import Organization
 
     class MyTask(GeocodeBatchTask):
-        def combine(self):
-            pass
+        pass
 
     geo = MyTask(job_def='', job_name='', job_queue='', region_name='',
                  city_col=Organization.city, country_col=Organization.country,
--- nesta/production/routines/meetup/health_tagging/health_meetup_es_task.py
+++ nesta/production/routines/meetup/health_tagging/health_meetup_es_task.py
@@ -5,33 +5,40 @@ Meetup data to elasticsearch
 Luigi routine to load the Meetup Group data from MYSQL into Elasticsearch.
 '''
 
+import logging
+import luigi
+import datetime
 from nesta.production.luigihacks.misctools import find_filepath_from_pathstub as f3p
-
 from nesta.production.luigihacks.estask import ElasticsearchTask
-from nesta.production.orms.meetup import Group
+from nesta.production.orms.meetup_orm import Group
 from nesta.production.luigihacks.batchgeocode import GeocodeBatchTask
-from nesta.production.meetup.health_tagging.topic_discovery_task import TopicDiscoveryTask
+from nesta.production.routines.meetup.health_tagging.topic_discovery_task import TopicDiscoveryTask
+from nesta.production.routines.meetup.health_tagging.group_details_task import GroupDetailsTask
+
 
 class MeetupHealthElasticsearchTask(ElasticsearchTask):
+    core_categories = luigi.ListParameter()
+    members_perc = luigi.IntParameter()
+    topic_perc = luigi.IntParameter()
+
     def requires(self):
-        yield GeocodeBatchTask(date=self.date,
-                               _routine_id=self._routine_id,
+        yield GeocodeBatchTask(_routine_id=self.routine_id,
                                test=self.test,
                                db_config_env=self.db_config_env,
                                city_col=Group.city,
                                country_col=Group.country,
-                               insert_batch_size=10000,
-                               env_files=[f3p("nesta/nesta/"),
+                               country_is_iso2=True,
+                               env_files=[f3p("nesta/"),
                                           f3p("config/mysqldb.config"),
                                           f3p("config/crunchbase.config")],
                                job_def="py36_amzn1_image",
-                               job_name=f"HealthMeetupGeocodeBatchTask-{self._routine_id}",
+                               job_name=f"HealthMeetupGeocodeBatchTask-{self.routine_id}",
                                job_queue="HighPriority",
                                region_name="eu-west-2",
                                poll_time=10,
                                memory=4096,
                                max_live_jobs=2)
-
+        
         yield TopicDiscoveryTask(routine_id=self.routine_id,
                                  core_categories=self.core_categories,
                                  members_perc=self.members_perc,
@@ -52,10 +59,11 @@ class RootTask(luigi.WrapperTask):
     drop_and_recreate = luigi.BoolParameter(default=False)
 
     def requires(self):
+        logging.getLogger().setLevel(logging.INFO)
         routine_id = (f"{self.date}-{'--'.join(self.core_categories)}"
                       f"-{self.members_perc}-{self.topic_perc}-{self.production}")
         yield MeetupHealthElasticsearchTask(routine_id=routine_id,                                            
-                                            date=self.date
+                                            date=self.date,
                                             process_batch_size=10000,
                                             insert_batch_size=10000,
                                             drop_and_recreate=self.drop_and_recreate,
@@ -70,7 +78,7 @@ class RootTask(luigi.WrapperTask):
                                             test=not self.production,
                                             intermediate_bucket='nesta-production-intermediate',
                                             batchable=f3p("batchables/meetup/topic_tag_elasticsearch"),
-                                            env_files=[f3p("nesta/nesta/"),
+                                            env_files=[f3p("nesta/"),
                                                        f3p("config/mysqldb.config"),
                                                        f3p("schema_transformations/meetup.json"),
                                                        f3p("config/elasticsearch.config")],
