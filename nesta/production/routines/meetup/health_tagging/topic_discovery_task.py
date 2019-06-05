74d192b6ec0f593c5b296c878c8f467018a02969
Joel Klinger <joel.klinger@nesta.org.uk>
2019-06-05 13:13:18 +0000

    Health tagging task implemented

---
+++ nesta/production/routines/meetup/health_tagging/topic_discovery_task.py
@@ -0,0 +1,70 @@
+'''
+Meetup topic discovery task
+===========================
+
+Task to automatically discover relevant topics from meetup data,
+defined as the most frequently occurring from a set of categories.
+'''
+
+import luigi
+import datetime
+import json
+
+from nesta.production.luigihacks import s3
+from nesta.production.orms.orm_utils import get_mysql_engine
+from nesta.packages.meetup.meetup_utils import get_members_by_percentile
+from nesta.packages.meetup.meetup_utils import get_core_topics
+
+S3PREFIX = "s3://nesta-production-intermediate"
+
+
+class TopicDiscoveryTask(luigi.Task):
+    '''An intermediate task which increments the age of the muppets by 1 year.
+
+    Args:
+
+    '''
+    db_config_env = luigi.Parameter()
+    routine_id = luigi.Parameter()
+    core_categories = luigi.ListParameter()
+    members_perc = luigi.IntParameter(default=10)
+    topic_perc = luigi.IntParameter(default=10)
+    test = luigi.BoolParameter(default=True)
+
+    def output(self):
+        '''Points to the S3 Target'''
+        return s3.S3Target(f"{S3PREFIX}/meetup-topics-{self.routine_id}.json")
+
+    def run(self):
+        '''Extract the topics of interest'''
+        database = 'dev' if self.test else 'production'
+        engine = get_mysql_engine(self.db_config_env, 'mysqldb', database)
+        members_limit = get_members_by_percentile(engine, perc=self.members_perc)
+        topics = get_core_topics(engine,
+                                 core_categories=self.core_categories,
+                                 members_limit=members_limit,
+                                 perc=self.topic_perc)
+
+        # Write the intermediate output
+        with self.output().open('wb') as outstream:
+            outstream.write(json.dumps(list(topics)).encode('utf8'))
+
+class RootTask(luigi.WrapperTask):
+    production = luigi.BoolParameter(default=False)
+    date = luigi.DateParameter(default=datetime.datetime.today())
+    core_categories = luigi.ListParameter(default=["community-environment",
+                                                   "health-wellbeing",
+                                                   "fitness"])
+    members_perc = luigi.IntParameter(default=10)
+    topic_perc = luigi.IntParameter(default=99)
+    db_config_env = luigi.Parameter("MYSQLDB")
+
+    def requires(self):
+        routine_id = (f"{self.date}-{'--'.join(self.core_categories)}"
+                      f"-{self.members_perc}-{self.topic_perc}-{self.production}")
+        yield TopicDiscoveryTask(routine_id=routine_id,
+                                 core_categories=self.core_categories,
+                                 members_perc=self.members_perc,                                 
+                                 topic_perc=self.topic_perc,
+                                 db_config_env=self.db_config_env,
+                                 test=not self.production)
