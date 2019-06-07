f4e77e449153fdadea94db30f82566dce3d7e4c9
Russell Winch <russell.winch@nesta.org.uk>
2019-06-07 19:43:05 +0100

    Switch CMD to ENTRYPOINT. adjust bash script to pass on any parameters.

--- docker/run.sh
+++ docker/run.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
-luigid &
-cd nesta/production/routines/arxiv
 
-# testing with one task. This script should be generated on the fly
-python -m luigi --module arxiv_iterative_root_task RootTask --date 2019-04-16
-
-
+# pass any arguments straight on to luigi
+luigi --local-scheduler "$@"
