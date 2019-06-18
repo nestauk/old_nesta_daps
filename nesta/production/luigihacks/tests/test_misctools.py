3e49fa6c9174c6c88e56c82b2296307002eaf351
russwinch <russell.winch@nesta.org.uk>
2019-06-18 18:58:44 +0100

    Initial containerisation of Luigi (#106)
    * Luigi running in a container with local files copied in.
    
    * Create docker-compose for arxlive.
    
    * Add basic .dockerignore file.
    
    * Add an extra level of iteration so find_filepath_from_pathstub doesn't exit early when 1 level from HOME. 
    
    * Change relative paths to absolute for arxiv pipeline so calling from outside folder works.
    
    * Add documentation for containerised luigi.
    
    * Tidy up Dockerfile and implement arg for python version.

--- nesta/production/luigihacks/tests/test_misctools.py
+++ nesta/production/luigihacks/tests/test_misctools.py
@@ -2,16 +2,16 @@ from unittest import TestCase
 from nesta.production.luigihacks.misctools import get_config
 from nesta.production.luigihacks.misctools import find_filepath_from_pathstub
 
+
 class TestMiscTools(TestCase):
-    
     def test_get_config(self):
         get_config("mysqldb.config", "mysqldb")
         with self.assertRaises(KeyError):
-            get_config("mysqldb.config", "mysqld")
+            get_config("mysqldb.config", "invalid")
         with self.assertRaises(KeyError):
-            get_config("mysqldb.confi", "mysqldb")
-        
+            get_config("not_found.config", "mysqldb")
+
     def test_find_filepath_from_pathstub(self):
         find_filepath_from_pathstub("nesta/packages")
         with self.assertRaises(FileNotFoundError):
-            find_filepath_from_pathstub("nesta/package")            
+            find_filepath_from_pathstub("nesta/package")
