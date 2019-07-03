9637ea26222053fe725712f28d86eeeb82276774
Joel Klinger <joel.klinger@nesta.org.uk>
2019-07-03 15:45:48 +0000

    removed circular automl dependencies; dynamic renaming of MLTask via factory method; input Task now settable via parameters

--- nesta/production/batchables/nlp/ngrammer/run.py
+++ nesta/production/batchables/nlp/ngrammer/run.py
@@ -14,7 +14,6 @@ import json
 def run():
     # Extract environmental variables
     s3_path_in = os.environ['BATCHPAR_s3_path_in']
-    s3_path_out = os.environ["BATCHPAR_outinfo"]
     first_index = int(os.environ['BATCHPAR_first_index'])
     last_index = int(os.environ['BATCHPAR_last_index'])
 
@@ -33,8 +32,9 @@ def run():
                    for k, v in row.items()}
         processed.append(new_row)
 
-    # Mark the task as done and save the data
-    if s3_path_out != "":
+    # Mark the task as done and save the data    
+    if "BATCHPAR_outinfo" in os.environ:
+        s3_path_out = os.environ["BATCHPAR_outinfo"]
         s3 = boto3.resource('s3')
         s3_obj = s3.Object(*parse_s3_path(s3_path_out))
         s3_obj.put(Body=json.dumps(processed))
@@ -43,8 +43,13 @@ def run():
 if __name__ == "__main__":
     # Local testing
     if "BATCHPAR_outinfo" not in os.environ:
-        os.environ['BATCHPAR_s3_path_in'] = "s3://clio-data/gtr/raw_data/gtr_raw_data.json"
-        os.environ['BATCHPAR_outinfo'] = "s3://clio-data/gtr/intermediate/gtr/raw_data_to_gtr/processed_data_ngram.0-10.json"
-        os.environ['BATCHPAR_first_index'] = '0'
-        os.environ["BATCHPAR_last_index"] = '10'
+        os.environ["BATCHPAR_s3_path_in"] = "s3://nesta-arxlive/raw-inputs/2019-06-18/data.0-True.json"
+        os.environ["BATCHPAR_last_index"] = "-1"
+        os.environ["BATCHPAR_first_index"] = "0"
+        os.environ["BATCHPAR_S3FILE_TIMESTAMP"] = "run-1560876797721923813.zip"
+        
+        # os.environ['BATCHPAR_s3_path_in'] = "s3://clio-data/gtr/raw_data/gtr_raw_data.json"
+        # os.environ['BATCHPAR_outinfo'] = "s3://clio-data/gtr/intermediate/gtr/raw_data_to_gtr/processed_data_ngram.0-10.json"
+        # os.environ['BATCHPAR_first_index'] = '0'
+        # os.environ["BATCHPAR_last_index"] = '10'
     run()
