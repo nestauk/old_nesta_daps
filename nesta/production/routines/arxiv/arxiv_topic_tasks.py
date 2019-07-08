c386b407ae3813377d64de4ad8d1efdd951e010f
Joel Klinger <joel.klinger@nesta.org.uk>
2019-07-08 17:31:15 +0000

    Written ORMs for topics

--- nesta/production/routines/arxiv/arxiv_topic_tasks.py
+++ nesta/production/routines/arxiv/arxiv_topic_tasks.py
@@ -8,8 +8,15 @@ from sqlalchemy import or_
 from nesta.production.luigihacks import s3
 from nesta.production.orms.arxiv_orm import article_categories as ArtCat
 from nesta.production.orms.arxiv_orm import Article
+from nesta.production.orms.arxiv_orm import CorExTopic
+from nesta.production.orms.arxiv_orm import ArticleTopic
+from nesta.production.orms.arxiv_orm import Base
 from nesta.production.luigihacks.automl import AutoMLTask
 from nesta.production.orms.orm_utils import get_mysql_engine, db_session
+from nesta.production.orms.orm_utils import insert_data
+
+from nesta.production.luigihacks import misctools
+from nesta.production.luigihacks.mysqldb import MySqlTarget
 
 
 THIS_PATH = os.path.dirname(os.path.realpath(__file__))
@@ -61,6 +68,67 @@ class PrepareArxivS3Data(luigi.Task):
         f.write(str(i).encode("utf-8"))
         f.close()
 
+class WriteTopicTask(luigi.Task):
+    s3_path_prefix = luigi.Parameter()
+    raw_s3_path_prefix = luigi.Parameter()
+    data_path = luigi.Parameter()
+    date = luigi.DateParameter()
+    db_config_path = luigi.Parameter('mysqldb.config')
+    db_conf_env = luigi.Parameter(default="MYSQLDB")
+    test = luigi.BoolParameter()
+
+    def output(self):
+        '''Points to the output database engine'''
+        db_config = misctools.get_config(self.db_config_path, "mysqldb")
+        db_config["database"] = 'dev' if self.test else 'production'
+        db_config["table"] = "arXlive <dummy>"  # Note, not a real table
+        update_id = "ArxivTopicTask_{}".format(self.date)
+        return MySqlTarget(update_id=update_id, **db_config)
+
+
+    def requires(self):
+        return AutoMLTask(s3_path_prefix=self.s3_path_prefix,
+                          task_chain_filepath=CHAIN_PARAMETER_PATH,
+                          test=self.test,
+                          input_task=PrepareArxivS3Data,
+                          input_task_kwargs={'s3_path_out':self.data_path,
+                                             'test':self.test})
+
+        
+    def run(self):
+        # Load the input data (note the input contains the path
+        # to the output)
+
+        _body = self.input().open("rb")
+        _filename = _body.read().decode('utf-8')
+        obj = s3.S3Target(f"{self.raw_s3_path_prefix}/"
+                          f"{_filename}").open('rb')
+        data = json.load(obj)        
+
+        # Get DB connections and settings
+        database = 'dev' if self.test else 'production'
+        engine = get_mysql_engine(self.db_conf_env, 'mysqldb', database)
+
+        # Insert the topic names data
+        topics = [{'id':int(topic_name.split('_')[-1]), 'terms':terms}
+                  for topic_name, terms in
+                  data['data']['topic_names'].items()]
+        insert_data(self.db_conf_env, 'mysqldb', database, 
+                    Base, CorExTopic, topics, low_memory=True)
+
+        # Insert article topic weight data
+        for row in data['data']['rows']:
+            article_id = row.pop('id')
+            topic_articles += [{'topic_id': int(topic_name.split('_')[-1]),
+                                'weight': weight, 'article_id': article_id}
+                               for topic_name, weight in row.items()]
+        insert_data(self.db_config_env, 'mysqldb', database,
+                    Base, ArticleTopic, topic_articles,
+                    low_memory=False)
+
+        # Touch the output
+        self.output.touch()
+
 
 class TopicRootTask(luigi.WrapperTask):
     production = luigi.BoolParameter(default=False)
@@ -70,13 +138,13 @@ class TopicRootTask(luigi.WrapperTask):
 
     def requires(self):
         logging.getLogger().setLevel(logging.INFO)
+        test = not self.production
+        s3_path_prefix=(f"{self.s3_path_prefix}/"
+                        f"automl/{self.date}")
         data_path = (f"{self.s3_path_prefix}/"
                      f"{self.raw_data_path}/{self.date}")
-        test = not self.production
-        yield AutoMLTask(s3_path_prefix=(f"{self.s3_path_prefix}/"
-                                         f"automl/{self.date}"),
-                         task_chain_filepath=CHAIN_PARAMETER_PATH,
-                         test=test,
-                         input_task=PrepareArxivS3Data,
-                         input_task_kwargs={'s3_path_out':data_path,
-                                            'test':test})
+        return WriteTopicTask(raw_s3_path_prefix=self.s3_path_prefix,
+                              s3_path_prefix=s3_path_prefix,
+                              data_path=data_path,
+                              date=self.date,
+                              test=test)
