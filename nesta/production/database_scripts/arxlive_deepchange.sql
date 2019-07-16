8c9bd9c738ff2c7a2cc05a5d09b399e5ea795d91
Russell Winch <russell.winch@nesta.org.uk>
2019-07-16 17:27:40 +0100

    Add topics and include arxiv category names in sql.
    - Rename category id and add descriptions so they can be used in the
    chart, in place of ids.
    - Apply join to filter only those with topics.
    - Rename JOIN to INNER JOIN for clarity.
    
    *WHERE needs to remain otherwise other categories are picked where an
    article has multiple.*

--- nesta/production/database_scripts/arxlive_deepchange.sql
+++ nesta/production/database_scripts/arxlive_deepchange.sql
@@ -1,31 +1,34 @@
 SELECT
-     a.id as article_id,
-     a.created AS article_created,
-     a.updated AS article_updated,
-     a.title AS article_title,
-     a.citation_count,
-     GROUP_CONCAT(distinct c.id) AS arxiv_categories,
-     ai.is_multinational,
-     gi.city AS institute_city,
-     gi.state AS institute_state,
-     gi.country AS institute_country
+    a.id as article_id,
+    a.created AS article_created,
+    a.updated AS article_updated,
+    a.title AS article_title,
+    a.citation_count,
+    GROUP_CONCAT(DISTINCT c.id SEPARATOR "|") AS arxiv_category_ids,
+    GROUP_CONCAT(DISTINCT c.description SEPARATOR "|") AS arxiv_category_descs,
+    ai.is_multinational,
+    gi.city AS institute_city,
+    gi.state AS institute_state,
+    gi.country AS institute_country
 
- FROM arxiv_articles a
- JOIN arxiv_article_categories ac ON ac.article_id = a.id
- JOIN arxiv_categories c ON c.id = ac.category_id
- LEFT JOIN arxiv_article_institutes ai ON ai.article_id = a.id AND ai.matching_score >= 0.9
- LEFT JOIN grid_institutes gi ON gi.id = ai.institute_id
+FROM arxiv_articles a
 
- WHERE
-     c.id = "stat.ML" OR c.id like "cs.%%"
+INNER JOIN arxiv_article_categories ac ON ac.article_id = a.id
+INNER JOIN arxiv_categories c ON c.id = ac.category_id
+INNER JOIN arxiv_article_corex_topics ct on ct.article_id = a.id
+LEFT JOIN arxiv_article_institutes ai ON ai.article_id = a.id AND ai.matching_score >= 0.9
+LEFT JOIN grid_institutes gi ON gi.id = ai.institute_id
 
- GROUP BY
-     a.id,
-     a.created,
-     a.updated,
-     a.title,
-     a.citation_count,
-     ai.is_multinational,
-     gi.city,
-     gi.state,
-     gi.country
+WHERE c.id = "stat.ML" OR c.id LIKE "cs.%%"
+
+GROUP BY
+    a.id,
+    a.created,
+    a.updated,
+    a.title,
+    a.citation_count,
+    ai.is_multinational,
+    gi.city,
+    gi.state,
+    gi.country
+;
