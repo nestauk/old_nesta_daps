55baeb8a30e677796268380dddd17adb181dea56
dede95 <adotu95@gmail.com>
2019-08-08 20:23:12 +0100

    New function and update in general.py

--- nesta/packages/dq/general.py
+++ nesta/packages/dq/general.py
@@ -3,14 +3,13 @@ import numpy as np
 import itertools
 
 def missing_values(input):
-
     """missing_values
 
     Args:
         input (:obj:`iter` of :obj:`obj`): A sequence of objects.
 
     Returns:
-        missing_count_df (:obj:`pandas.core.series.Series`): A series of column nme (index) and missing value frequency (column)
+        missing_count_df (:obj:`pandas.core.series.Series`): A series with column names (index) and missing value frequency (column)
 
 
     """
@@ -18,8 +17,24 @@ def missing_values(input):
     missing_count_df = df.isnull().sum().sort_values(ascending=False)
     return missing_count_df
 
-def missing_value_column_count(input):
+def missing_value_percentage_column_count(input):
+    """missing_value_percentage_column_count
 
+    Args:
+        input (:obj:`dict`): A dictionary of
+
+    Returns:
+        out_counts (:obj:`pandas.core.series.Series`): A series with mising value percentage ranges (index) and column frequency (column)
+
+    """
+
+    missing_bins = [i for i in range(100+1) if i%10 == 0]
+    out= pd.cut((pd.Series(input)/total_length).apply(lambda x: round(x*100,5)),
+                                                         bins=missing_bins, include_lowest=True)
+    out_counts = out.value_counts(sort=False)
+
+    return out_counts
+def missing_value_column_count(input):
     """missing_value_column_count
 
     Args:
@@ -37,8 +52,7 @@ def missing_value_column_count(input):
     return out
 
 def missing_value_count_pair_both(input):
-#include here or in script itself
-    """missing_value_column_count
+    """missing_value_count_pair_both
 
     Args:
         input (:obj:`iter` of :obj:`obj`): A multi-dimensional nested sequence of objects.
@@ -69,7 +83,7 @@ def missing_value_count_pair_both(input):
     return both_null_dict
 
 def missing_value_count_pair_either(input):
-    """missing_value_column_count
+    """missing_value_count_pair_either
 
     Args:
         input (:obj:`iter` of :obj:`obj`): A multi-dimensional nested sequence of objects.
@@ -102,13 +116,13 @@ def missing_value_count_pair_either(input):
     return either_null_dict
 
 def melt_dict_to_df(calc_dict):
-    """missing_value_column_count
+    """melt_dict_to_df
 
     Args:
         calc_dict (:obj:`dict`): Dictionary object with 2-element tuple keys and 1-dimensional values.
 
     Returns:
-        heat_Df (:obj:`tuple`): A dataframe with the elements of the tuple keys as indices and columns and the values as the dictionary values.
+        df (:obj:`tuple`): A dataframe with the elements of the tuple keys as indices and columns and the values as the dictionary values.
 
     """
     ser = pd.Series(list(calc_dict.values()),
