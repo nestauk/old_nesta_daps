e5a4d388130a78a67e7d8251a1511704a465e217
George Richardson <georgerichardson@posteo.net>
2019-08-14 15:59:36 +0100

    add run and output for mesh join task

--- nesta/production/routines/health_data/nih_data/mesh_join_task.py
+++ nesta/production/routines/health_data/nih_data/mesh_join_task.py
@@ -6,12 +6,15 @@ import logging
 import luigi
 import re
 
-from nesta.production.orms.orm_utils import get_mysql_engine, db_session, exists, insert_data
-from nesta.production.orms.mesh_orm import MeshTerms
+from nesta.production.orms.orm_utils import (get_mysql_engine, db_session, 
+        exists, insert_data, db_session)
+from nesta.production.orms.mesh_orm import MeshTerms, Base
 from nesta.production.orms.nih_orm import Projects
 from nesta.production.luigihacks.mysqldb import MySqlTarget
+from nesta.production.luigihacks.misctools import get_config
 
-from nesta.packages.health_data.process_mesh import retrieve_mesh_terms, format_mesh_terms
+from nesta.packages.health_data.process_mesh import (retrieve_mesh_terms, 
+        format_mesh_terms)
 
 class MeshJoinTask(luigi.Task):
     '''Joins MeSH labels stored in S3 to NIH projects in MySQL.
@@ -24,7 +27,7 @@ class MeshJoinTask(luigi.Task):
 
     date = luigi.DateParameter()
     _routine_id = luigi.Parameter()
-    db_config_path = luigi.Parameter()
+    db_config_env = luigi.Parameter()
     
     @staticmethod
     def get_abstract_file_keys(bucket, key_prefix):
@@ -32,11 +35,13 @@ class MeshJoinTask(luigi.Task):
         s3bucket = s3.Bucket(bucket)
         return {o.key for o in s3bucket.objects.filter(Prefix=key_prefix)}
 
-    def requires():
-        pass
-
     def output():
-        pass
+        self.db_config_path = os.environ[self.db_config_env]
+        db_config = get_config(self.db_config_path, "mysqldb")
+        db_config['database'] = 'dev' if self.test else 'production'
+        db_config['table'] = "MeshTerms <dummy>"
+        update_id = "NihJoinMeshTerms_{}".format(self.date)
+        return MySqlTarget(update_id=update_id, **db_config)
 
     def run():
         db = 'production' if not self.test else 'dev'
@@ -44,14 +49,34 @@ class MeshJoinTask(luigi.Task):
         bucket = 'innovation-mapping-general'
         key_prefix = 'nih_abstracts_processed/mti'
         keys = self.get_abstract_file_keys(bucket, key_prefix)
+        
+        engine = get_mysql_engine(self.db_config_env, 'mysqldb' db)
+        session = db(engine)
+        
+        association_table = Base.metadata.tables['nih_mesh_terms']
+        docs_done = {d.project_id for d in session.query(association_table).distinct()}
+
+        mesh_terms = session.query(MeshTerms.id, MeshTerms.term).all()
+        mesh_terms = {m.term: m.id for m in mesh_terms}
+        
         for key in keys:
             df_mesh = retrieve_mesh_terms(bucket, key)
             doc_terms = format_mesh_terms(df_mesh)
-
+            data = []
             for doc, terms in doc_terms.items():
-                for term in terms:
-                    if not exists(MeshTerms, {'term': term}):
-                        insert_data()
-
+                doc_terms = []
+                if doc in docs_done:
+                    continue
+                else:
+                    for term in terms:
+                        if term in mesh_terms:
+                            term_id = mesh_terms[term]
+                        else:
+                            objs = insert_data(self.db_config, 'mysqldb', db,
+                                    Base, MeshTerms, [{'term': term}])
+                            term_id = objs[0].id
+                        doc_terms.append({'project_id': doc, 'mesh_term_id': term_id})
+                    insert_data(self.db_config, 'mysqldb', db,
+                        Base, association_table, doc_terms)
 
 
