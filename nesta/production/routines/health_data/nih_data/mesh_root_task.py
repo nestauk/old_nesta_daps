47b90d3c5d10fc5d53af30dad9a4059c5f663136
George Richardson <georgerichardson@posteo.net>
2019-08-15 10:52:11 +0100

    fix pathstub

--- nesta/production/routines/health_data/nih_data/mesh_root_task.py
+++ nesta/production/routines/health_data/nih_data/mesh_root_task.py
@@ -1,7 +1,7 @@
 import luigi
 import datetime
 import logging
-from nesta.production.luigihacks.misctools import find_filepath_from_pathstub as f3p
+from nesta.production.luigihacks.misctools import f3p as f3p
 
 from nesta.production.routines.health_data.nih_data.mesh_join_task import MeshJoinTask
 
@@ -19,11 +19,11 @@ class RootTask(luigi.WrapperTask):
                 _routine_id=_routine_id,
                 db_config_path=self.db_config_path,
                 test=(not self.production),
-                batchable=find_filepath_from_pathstub("batchables/health_data/nih_mesh_join"),
-                env_files=[find_filepath_from_pathstub("nesta/nesta/"),
-                           find_filepath_from_pathstub("config/mysqldb.config"),
-                           find_filepath_from_pathstub("config/elasticsearch.config"),
-                           find_filepath_from_pathstub("nih.json")],
+                batchable=f3p("batchables/health_data/nih_mesh_join"),
+                env_files=[f3p("nesta/nesta/"),
+                           f3p("config/mysqldb.config"),
+                           f3p("config/elasticsearch.config"),
+                           f3p("nih.json")],
                 job_def="py36_amzn1_image",
                 job_name="AbstractsMeshTask-%s" % _routine_id,
                 job_queue="HighPriority",
