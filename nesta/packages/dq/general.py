99b672668b14576f938411dd10c7f346e129f16e
dede95 <adotu95@gmail.com>
2019-08-16 18:15:49 +0100

    Corrections in functions, changes in docstrings, removed missing_value_column_count

--- nesta/packages/dq/general.py
+++ nesta/packages/dq/general.py
@@ -2,70 +2,63 @@ import pandas as pd
 import numpy as np
 import itertools
 
-def missing_values(input):
+def missing_values(data):
     """missing_values
+    Calculates the number of missing values.
 
     Args:
-        input (:obj:`iter` of :obj:`obj`): A sequence of objects.
+        data (:obj:`iter` of :obj:`obj`): A sequence of objects.
 
     Returns:
         missing_count_df (:obj:`pandas.core.series.Series`): A series with column names (index) and missing value frequency (column)
 
 
     """
-    df = pd.DataFrame(input)
+    df = pd.DataFrame(data)
     missing_count_df = df.isnull().sum().sort_values(ascending=False)
     return missing_count_df
 
-def missing_value_percentage_column_count(input):
+def missing_value_percentage_column_count(data):
     """missing_value_percentage_column_count
+    Calculates the number of columns with a percentage of missing values.
 
     Args:
-        input (:obj:`dict`): A dictionary of
+        data (:obj:`pandas.DataFrame`): A sequence of objects.
 
     Returns:
-        out_counts (:obj:`pandas.core.series.Series`): A series with mising value percentage ranges (index) and column frequency (column)
+        out_counts (:obj:`pandas.DataFrame`): A dataframe with mising value percentage ranges (index) and column frequency (column)
 
     """
 
+    df = pd.DataFrame(data)
+    total_length = len(df)
+
+    missingvalue_df = missing_values(df)
     missing_bins = [i for i in range(100+1) if i%10 == 0]
-    out= pd.cut((pd.Series(input)/total_length).apply(lambda x: round(x*100,5)),
+    out= pd.cut((pd.Series(missingvalue_df)/total_length).apply(lambda x: round(x*100,5)),
                                                          bins=missing_bins, include_lowest=True)
+    # print(missingvalue_df)
+    # out = np.histogram(((pd.Series(missingvalue_df)/total_length)*100), bins=range(0, 101, 10))
     out_counts = out.value_counts(sort=False)
+    out_counts_df = pd.DataFrame({'intervals': out_counts.index, 'frequency': out_counts.values})
 
-    return out_counts
-def missing_value_column_count(input):
-    """missing_value_column_count
+    return out_counts_df
 
-    Args:
-        input (:obj:`iter` of :obj:`obj`): A sequence of objects.
-
-    Returns:
-        out (:obj:`tuple`): A tuple of two lists- first list with the frequency, second list the bins
-
-    """
-
-    df = pd.DataFrame(input)
-
-    data_frame_missing = df.isnull().sum() / df.shape[0]
-    out = np.histogram(data_frame_missing, bins=range(0, 101, 10))
-    return out
-
-def missing_value_count_pair_both(input):
+def missing_value_count_pair_both(data):
     """missing_value_count_pair_both
 
     Args:
-        input (:obj:`iter` of :obj:`obj`): A multi-dimensional nested sequence of objects.
+        data (:obj:`iter` of :obj:`obj`): A multi-dimensional nested sequence of objects.
 
     Returns:
-        both_null_dict (:obj:`tuple`): A tuple of two lists- first list with the frequency, second list the bins
+        both_null_dict (:obj:`pandas.DataFrame`): A symmetrical dataframe with column names (index and columns) and boolean counts if both entries of pairwise columns are missing (values)
 
     """
-    data_frame = pd.DataFrame(input)
+    data_frame = pd.DataFrame(data)
     pair_list = []
     for pair in itertools.combinations_with_replacement(data_frame.columns,2):
         pair_list.append(pair)
-        #1min 30secs
+
     both_null_dict = {}
     for pair in pair_list:
         bool_1 = pd.isnull(data_frame[pair[0]])
@@ -80,20 +73,24 @@ def missing_value_count_pair_both(input):
             both_null_dict[(pair[0],pair[1])] = 0
             both_null_dict[(pair[1],pair[0])] = 0
 
-    return both_null_dict
+    ser = pd.Series(list(both_null_dict.values()),
+                  index=pd.MultiIndex.from_tuples(both_null_dict.keys()))
+    both_null_df = ser.unstack().fillna(0)
 
-def missing_value_count_pair_either(input):
+    return both_null_df
+
+def missing_value_count_pair_either(data):
     """missing_value_count_pair_either
 
     Args:
-        input (:obj:`iter` of :obj:`obj`): A multi-dimensional nested sequence of objects.
+        data (:obj:`iter` of :obj:`obj`): A multi-dimensional nested sequence of objects.
 
     Returns:
-        either_null_dict (:obj:`tuple`): A tuple of two lists- first list with the frequency, second list the bins
+        either_null_dict (:obj:`pandas.DataFrame`): A symmetrical dataframe with column names (index and columns) and boolean counts if either entries of pairwise columns are missing (values)
 
     """
 
-    data_frame = pd.DataFrame(input)
+    data_frame = pd.DataFrame(data)
     pair_list = []
     for pair in itertools.combinations_with_replacement(data_frame.columns,2):
         pair_list.append(pair)
@@ -113,20 +110,8 @@ def missing_value_count_pair_either(input):
             either_null_dict[(pair[0],pair[1])] = 0
             either_null_dict[(pair[1],pair[0])] = 0
 
-    return either_null_dict
+    ser = pd.Series(list(either_null_dict.values()),
+                  index=pd.MultiIndex.from_tuples(either_null_dict.keys()))
+    either_null_df = ser.unstack().fillna(0)
 
-def melt_dict_to_df(calc_dict):
-    """melt_dict_to_df
-
-    Args:
-        calc_dict (:obj:`dict`): Dictionary object with 2-element tuple keys and 1-dimensional values.
-
-    Returns:
-        df (:obj:`tuple`): A dataframe with the elements of the tuple keys as indices and columns and the values as the dictionary values.
-
-    """
-    ser = pd.Series(list(calc_dict.values()),
-                  index=pd.MultiIndex.from_tuples(calc_dict.keys()))
-    df = ser.unstack().fillna(0)
-
-    return df
+    return either_null_df
