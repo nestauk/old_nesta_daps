cdbc95778e611987131d7bf0fb2d6580d66ce2aa
George Richardson <georgerichardson@posteo.net>
2019-08-19 10:18:54 +0100

    remove empty files

--- nesta/packages/innovation/novelty.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from rhodonite.utils.graph import subgraph_eprop_agg
-
-def novelty_score(g, vertices, k_eprop):
-    return subgraph_eprop_agg(g, vertices, k_eprop, aggfuncs=[np.mean])[0]
