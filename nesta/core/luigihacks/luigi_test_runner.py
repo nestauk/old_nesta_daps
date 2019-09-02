e691cb20f8329b53c35aa3de717c10b29b401678
Russell Winch <russell.winch@nesta.org.uk>
2019-09-02 18:17:27 +0100

    Create test_runner top level api

---
+++ nesta/core/luigihacks/luigi_test_runner.py
@@ -0,0 +1,101 @@
+import glob
+import logging
+import re
+
+
+def find_python_files(start_directory):
+    """Locates all .py files recursively from a supplied start path.
+
+    Args:
+        start_directory(str): path to start searching from
+
+    Returns:
+        (list): all found files
+    """
+    path = f"{start_directory}/**/*.py"
+    python_files = glob.glob(path, recursive=True)
+
+    return python_files
+
+
+def contains_root_task(python_file):
+    pass
+
+
+def find_root_tasks(start_directory):
+    """Locates python files containing root tasks and returns them in module format so
+    they can be run from the Luigi cli.
+
+    Args:
+        start_directory(str): path to the folder containing routines
+
+    Returns:
+        (list): module style paths for each file containing a RootTask
+    """
+    def repl(match):
+        """Forward slashes are replaced with '.' everything else is removed, ie '.py'"""
+        if match.group(0) == '/':
+            return '.'
+
+    rex = re.compile(r'\/|\.py')
+    modules = []
+    for python_file in find_python_files(start_directory):
+        if not contains_root_task(python_file):
+            continue
+
+        # convert to module_format
+        reformatted = re.sub(rex, repl, python_file)
+        modules.append(reformatted)
+
+    return modules
+
+
+def build_docker_image(dockerfile='docker/Dockerfile', **kwargs):
+    # client = docker.from_env()
+    # use low level api and capture the logs
+    pass
+
+
+def recreate_test_database():
+    # ensures data is all cleared down
+    pass
+
+
+def run_luigi_pipeline(module, **kwargs):
+    # launches the pipeline
+    pass
+
+
+def luigi_test_runner(start_directory,
+                      dockerfile='docker/Dockerfile',
+                      branch='dev',
+                      luigi_kwargs=None):
+    """Identifies all Luigi pipelines and runs them in Docker for end-to-end testing.
+
+    Args:
+        start_directory(str): path to the folder containing routines
+        dockerfile(str): path to the Dockerfile to use to build the image
+        branch(str): name or tag of the branch to use when running pipelines
+        luigi_kwargs(dict): arguments to pass to Luigi tasks eg date
+    """
+    logging.info(f"Building docker image on branch: {branch}")
+    build_docker_image(branch=branch)
+
+    root_tasks = find_root_tasks(start_directory)
+    logging.info(f"Found {len(root_tasks)} pipelines to run")
+
+    for task in root_tasks:
+        logging.debug("Recreating database")
+        recreate_test_database()
+        try:
+            logging.info(f"Running pipeline: {task}")
+            run_luigi_pipeline(task)
+        except Exception as e:
+            logging.error(e)
+
+
+if __name__ == '__main__':
+    log_stream_handler = logging.StreamHandler()
+    logging.basicConfig(handlers=[log_stream_handler, ],
+                        level=logging.INFO,
+                        format="%(asctime)s:%(levelname)s:%(message)s")
---
+++ nesta/core/luigihacks/tests/test_luigi_test_runner.py
@@ -0,0 +1,117 @@
+import pytest
+from unittest import mock
+
+from nesta.core.luigihacks.luigi_test_runner import luigi_test_runner
+from nesta.core.luigihacks.luigi_test_runner import find_root_tasks
+from nesta.core.luigihacks.luigi_test_runner import find_python_files
+
+
+@mock.patch('nesta.core.luigihacks.luigi_test_runner.run_luigi_pipeline', autospec=True)
+@mock.patch('nesta.core.luigihacks.luigi_test_runner.recreate_test_database', autospec=True)
+@mock.patch('nesta.core.luigihacks.luigi_test_runner.build_docker_image', autospec=True)
+@mock.patch('nesta.core.luigihacks.luigi_test_runner.find_root_tasks', autospec=True)
+class TestTestRunner:
+    @pytest.fixture
+    def modules_to_find(self):
+        return ['routines.mag.mag_root_task',
+                'routines.crunchbase.crunchbase_root_task',
+                'routines.nih.nih_root_task']
+
+    def test_find_root_tasks_is_called_correctly(self,
+                                                 mocked_find_root_tasks,
+                                                 mocked_build_image,
+                                                 mocked_recreate_database,
+                                                 mocked_run_pipeline):
+        luigi_test_runner(start_directory='routines')
+
+        mocked_find_root_tasks.assert_called_once_with(start_directory='routines')
+
+    def test_build_docker_image_is_called_with_specified_branch(self,
+                                                                mocked_find_root_tasks,
+                                                                mocked_build_image,
+                                                                mocked_recreate_database,
+                                                                mocked_run_pipeline):
+        luigi_test_runner(start_directory='routines', branch='new_feature')
+
+        mocked_build_image.assert_called_once_with(branch='new_feature')
+
+    def test_recreate_database_is_called_correctly(self,
+                                                   mocked_find_root_tasks,
+                                                   mocked_build_image,
+                                                   mocked_recreate_database,
+                                                   mocked_run_pipeline,
+                                                   modules_to_find):
+        mocked_find_root_tasks.return_value = modules_to_find
+        luigi_test_runner(start_directory='routines')
+
+        assert mocked_recreate_database.call_count == len(modules_to_find)
+
+    def test_all_pipelines_are_run(self,
+                                   mocked_find_root_tasks,
+                                   mocked_build_image,
+                                   mocked_recreate_database,
+                                   mocked_run_pipeline,
+                                   modules_to_find):
+        mocked_find_root_tasks.return_value = modules_to_find
+        expected_result = [mock.call(module) for module in modules_to_find]
+
+        luigi_test_runner(start_directory='routines')
+
+        assert mocked_run_pipeline.mock_calls == expected_result
+
+    def test_all_pipelines_are_still_run_when_they_error(self,
+                                                         mocked_find_root_tasks,
+                                                         mocked_build_image,
+                                                         mocked_recreate_database,
+                                                         mocked_run_pipeline,
+                                                         modules_to_find):
+        mocked_find_root_tasks.return_value = modules_to_find
+        mocked_run_pipeline.side_effect = [ValueError, ZeroDivisionError, None]
+
+        luigi_test_runner(start_directory='routines')
+
+        assert mocked_run_pipeline.call_count == 3
+
+
+@mock.patch('nesta.core.luigihacks.luigi_test_runner.contains_root_task')
+@mock.patch('nesta.core.luigihacks.luigi_test_runner.find_python_files')
+class TestFindRootTasks:
+    @pytest.fixture
+    def paths_to_find(self):
+        return ['routines/mag/mag_root_task.py',
+                'routines/crunchbase/crunchbase_root_task.py',
+                'routines/nih/nih_root_task.py']
+
+    def test_all_found_tasks_are_returned_in_module_format(self,
+                                                           mocked_find_python,
+                                                           mocked_contains_root,
+                                                           paths_to_find):
+        mocked_find_python.return_value = paths_to_find
+        mocked_contains_root.return_value = True
+        expected_result = ['routines.mag.mag_root_task',
+                           'routines.crunchbase.crunchbase_root_task',
+                           'routines.nih.nih_root_task']
+
+        result = find_root_tasks(start_directory='routines')
+
+        assert result == expected_result
+
+    def test_files_not_contining_root_tasks_are_excluded(self,
+                                                         mocked_find_python,
+                                                         mocked_contains_root,
+                                                         paths_to_find):
+        mocked_find_python.return_value = paths_to_find
+        mocked_contains_root.side_effect = [True, False, True]
+
+        result = find_root_tasks(start_directory='routines')
+
+        assert len(result) == 2
+        assert 'routines.crunchbase.crunchbase_root_task' not in result
+
+
+@mock.patch('nesta.core.luigihacks.luigi_test_runner.glob.glob')
+class TestFindPythonFiles:
+    def test_glob_is_called_correctly(self, mocked_glob):
+        find_python_files('subfolder/routines')
+
+        mocked_glob.assert_called_once_with('subfolder/routines/**/*.py', recursive=True)
