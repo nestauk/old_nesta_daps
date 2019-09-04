6223bdd28347b56f172cfe2ae22051c0b532de51
George Richardson <georgerichardson@posteo.net>
2019-09-04 10:08:55 +0100

    add comments, docstrings and tidy

--- nesta/core/routines/health_data/nih_data/mesh_root_task.py
+++ nesta/core/routines/health_data/nih_data/mesh_root_task.py
@@ -6,6 +6,16 @@ from nesta.core.luigihacks.misctools import find_filepath_from_pathstub as f3p
 from nesta.core.routines.health_data.nih_data.mesh_join_task import MeshJoinTask
 
 class RootTask(luigi.WrapperTask):
+    '''A dummy root task, which collects the database configurations
+    and executes the central task.
+
+    Args:
+        date (datetime): Date used to label the outputs
+        db_config_env (str): Environment variable that points to the path to 
+            the MySQL database configuration
+        production (bool): Flag indicating whether running in testing
+                           mode (False, default), or production mode (True).
+    '''
 
     date = luigi.DateParameter(default=datetime.date.today())
     db_config_env = luigi.Parameter(default='MYSQLDB')
