4b975c9acffb8618f4fc4cfe2c54acdf423e7d03
Russell Winch <russell.winch@nesta.org.uk>
2019-09-06 15:00:48 +0100

    Add docker to requirements

--- make_requirements.sh
+++ make_requirements.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pipreqs  --ignore scripts,schemas,docs,.travis,.git --print . | sort -f > requirements.txt
+pipreqs  --ignore scripts,schemas,docs,.travis,.git,docker --print . | sort -f > requirements.txt
