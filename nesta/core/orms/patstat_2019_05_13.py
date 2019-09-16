924fbc50ccd09fba290eed270aa0a3867c151d51
Joel Klinger <joel.klinger@nesta.org.uk>
2019-09-16 13:16:32 +0000

    tidied import

--- nesta/core/orms/patstat_2019_05_13.py
+++ nesta/core/orms/patstat_2019_05_13.py
@@ -2,7 +2,7 @@
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
-from sqlalchemy.types import CHAR,VARCHAR,SMALLINT,INT,REAL,DATE,NVARCHAR
+from sqlalchemy.types import REAL,NVARCHAR,VARCHAR,INT,CHAR,SMALLINT,DATE
 
 Base = declarative_base()
 
