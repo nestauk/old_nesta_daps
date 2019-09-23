573cad6011fafd17b473a624c4a475a5617e8550
Joel Klinger <joel.klinger@nesta.org.uk>
2019-09-23 17:14:27 +0000

    updated slimmed launch and dockerfile

--- nesta/core/scripts/docker_recipes/py36_amzn1_image/launch.sh
+++ nesta/core/scripts/docker_recipes/py36_amzn1_image/launch.sh
@@ -1,51 +1,29 @@
 #!/bin/bash
 
-df -h
 # Install any other python packages which aren't picked up
 # in the requirements
-# sudo ls /usr/bin/pip*
-# sudo /usr/bin/pip-3.6 install awscli --upgrade --user
-# sudo /usr/bin/pip-3.6 install pyvirtualdisplay
 source activate py36
 which pip
 which python
 pip install awscli --upgrade --user
-df -h
 pip install lxml
-df -h
+conda clean -afy
 
 # Pull the batchable from S3
 echo "Getting file" ${BATCHPAR_S3FILE_TIMESTAMP}
 aws s3 cp s3://nesta-batch/${BATCHPAR_S3FILE_TIMESTAMP} run.zip
-df -h
 /usr/bin/unzip run.zip
-df -h
 rm run.zip  # clear up some space
-df -h
 cd run
 ls
 
 # Print out the caller id
 #aws sts get-caller-identity
 #aws iam list-roles
-#export WORLD_BORDERS="meetup/data/TM_WORLD_BORDERS_SIMPL-0.3.shp"
-
-# You could install anything else here as you wish, 
-# but you should really do this in the Dockerfile
-# sudo yum -y install wget
-# sudo yum -y install findutils
-
-# Install dependencies from the requirements file
-#sudo /usr/bin/pip-3.6 install -r requirements.txt
-#sudo /usr/bin/pip-3.6 install lxml
-
-sed -i '/tensorflow/d' requirements.txt  # remove TF from reqs since it's huge
+sed -i '/tensorflow/d' requirements.txt  # rm TF from reqs since it's huge
 pip install -r requirements.txt
-pip freeze
-df -h
-
-conda clean --all -y
-df -h
+#pip freeze
+conda clean -afy
 
 # Check the file exists and run it
 echo "Starting..."
