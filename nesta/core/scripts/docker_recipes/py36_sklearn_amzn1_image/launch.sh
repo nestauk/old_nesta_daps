f790bbfa97e10f3cfe3fe4bb96c3fe2856e9fb66
Joel Klinger <joel.alexander.klinger@gmail.com>
2019-09-26 13:54:58 +0100

    Vectorizer idxs fell out of sync, so added defaults (#187)
    * vectorizer idxs fell out of sync, so added defaults
    
    * removed mag change
    
    * removed task change
    
    * fixed translation change with looser req
    
    * slimmed down docker build
    
    * added launch file
    
    * added testing and docs

---
+++ nesta/core/scripts/docker_recipes/py36_sklearn_amzn1_image/launch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install any other python packages which aren't picked up
+# in the requirements
+source activate py36
+which pip
+which python
+pip install awscli --upgrade --user
+pip install lxml
+conda clean -afy
+
+# Pull the batchable from S3
+echo "Getting file" ${BATCHPAR_S3FILE_TIMESTAMP}
+aws s3 cp s3://nesta-batch/${BATCHPAR_S3FILE_TIMESTAMP} run.zip
+/usr/bin/unzip run.zip
+rm run.zip  # clear up some space
+cd run
+ls
+
+# Print out the caller id
+#aws sts get-caller-identity
+#aws iam list-roles
+sed -i '/tensorflow/d' requirements.txt  # rm TF from reqs since it's huge
+pip install -r requirements.txt
+#pip freeze
+conda clean -afy
+
+# Check the file exists and run it
+echo "Starting..."
+cat run.py &> /dev/null
+time python run.py
