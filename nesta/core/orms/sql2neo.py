80702bb4ab84043ff160becaccf63cf6c4a59d28
Karlis Kanders <beingkk@users.noreply.github.com>
2019-10-18 17:32:43 +0100

    sql to neo task, first try

--- nesta/core/orms/sql2neo.py
+++ nesta/core/orms/sql2neo.py
@@ -1,9 +1,60 @@
-from py2neo import NodeMatcher
-from py2neo.data import Node, Relationship
-from sqlalchemy.inspection import inspect
-from nesta.core.orms.orm_utils import get_class_by_tablename, object_to_dict
-from nesta.core.orms.cordis_orm import Base, Project
+from py2neo.data import Node, Relationship, NodeSelector
+from nesta.core.orms.orm_utils import object_to_dict
 import logging
 
 
-# Add stuff here
+def table_to_neo4j(engine, transaction,
+                   table=None, limit=None,
+                   is_node=True, node_label=None,
+                   rel_type=None):
+
+    # Check if everything that's required has been provided
+    assert table is not None, "No table was provided!"
+
+    assert ((is_node is True and node_label is not None) or
+            (is_node is False and rel_type is not None))
+
+    graph = transaction.graph
+
+    # Infer names of foreign key columns
+    fks = []
+    pks = []
+    for col in table.columns:
+        if col.foreign_keys:
+            fks.append(col.name) # store the name of fk column of cooc table
+            # for fk in col.foreign_keys: # store the name of id column
+                # pks.append(fk.column.table.primary_key.column.name)
+
+    # Check that we have only two columns with a foreign key
+    assert(len(fk_table_names)==2)
+
+    selector = NodeSelector(graph)
+
+    # Query table, iterate through rows and send to neo4j
+    for db, orm_instance in db_session_query(query=table, engine=engine,
+                                             limit=limit, offset=irow):
+        irow += 1
+        if irow == limit:
+            break
+
+        if irow % 500 == 0:
+            logging.info(f'Created {irow} new entities')
+
+        data_row = object_to_dict(orm_instance, shallow=True)
+
+        # If adding a node
+        if is_node:
+            graph.create(Node(node_label, **data_row))
+
+        # If adding an edge
+        else:
+            first_node = selector.select(node_label, id=data_row[fk[0]]).first()
+            second_node = selector.select(node_label, id=data_row[fk[1]]).first()
+
+            data_row.pop(fk[0])
+            data_row.pop(fk[1])
+
+            rel = Relationship(first_node, rel_type, second_node, **data_row)
+            graph.create(rel)
+
+    return irow
--- nesta/core/routines/reviews/reviews_neo_cooc_task.py
+++ nesta/core/routines/reviews/reviews_neo_cooc_task.py
@@ -4,25 +4,24 @@ CooccuranceTableNeo4jTask
 Task for turning a co-occurance table from SQL into a Neo4j graph.
 """
 
-from nesta.core.orms.cordis_orm import Base
+from nesta.core.orms.esco_orm import Skills, SkillCooccurrences
+from nesta.core.orms.sql2neo import table_to_neo4j
 from nesta.core.orms.orm_utils import db_session_query, get_mysql_engine
 from nesta.core.orms.orm_utils import graph_session
 from nesta.core.luigihacks.luigi_logging import set_log_level
 from nesta.core.luigihacks.misctools import get_config
-from eurito_daps.packages.cordis.cordis_neo4j import extract_name, orm_to_neo4j
-from eurito_daps.packages.cordis.cordis_neo4j import prepare_base_entities
 from nesta.core.luigihacks.mysqldb import MySqlTarget
 
 from datetime import datetime as dt
 import luigi
 import logging
 import os
-from py2neo import Graph
 
 
-class CooccuranceTableNeo4jTask(luigi.Task):
+class CooccurrenceTableNeo4jTask(luigi.Task):
     test = luigi.BoolParameter(default=True)
     date = luigi.DateParameter(default=dt.now())
+    graph_url = luigi.Parameter()
 
     def output(self):
         '''Points to the output database engine where the task is marked as done.
@@ -31,21 +30,54 @@ class CooccuranceTableNeo4jTask(luigi.Task):
         db_config = get_config(os.environ["MYSQLDB"], "mysqldb")
         db_config["database"] = 'dev' if self.test else 'production'
         db_config["table"] = "Cooc Neo4j <dummy>"  # Note, not a real table
-        update_id = "CooccuranceTableNeo4j_{}".format(self.date)
+        update_id = "CooccurrenceTableNeo4j_{}".format(self.date)
         return MySqlTarget(update_id=update_id, **db_config)
 
     def run(self):
-        # Add stuff here
-        
+
+        limit = 100 if self.test else None
+
+        # Set up the sql database connection
+        database = 'dev' if self.test else 'production'
+        logging.warning(f"Using {database} database")
+        self.engine = get_mysql_engine(self.db_config_env, 'mysqldb', database)
+
+        with graph_session(self.graph_url) as tx:
+
+            # Drop all neo4j data in advance
+            # (WARNING: this is a hack in lieu of proper db staging/versioning)
+            logging.info('Dropping all previous data')
+            tx.graph.delete_all()
+            for constraint in tx.run('CALL db.constraints'):
+                logging.info(f'Dropping constraint {constraint[0]}')
+                tx.run(f'DROP {constraint[0]}')
+
+            # Generate nodes from the Skills table
+            n = table_to_neo4j(engine=self.engine, transaction=tx,
+                               table=Skills,
+                               is_node=True, node_label='skill')
+
+            logging.info(f'Created {n} nodes')
+
+            # Generate relationships from the SkillCooccurrences table
+            n = table_to_neo4j(session=self.engine, transaction=tx,
+                               table=SkillCooccurrences, limit=limit,
+                               is_node=False, rel_type='HAS_SAME_OCCUPATIONS')
+
+            logging.info(f'Created {n} links')
+
         # Confirm the task is finished
+        logging.warning("Task complete")
         self.output().touch()
 
 
 class RootTask(luigi.WrapperTask):
     production = luigi.BoolParameter(default=False)
     date = luigi.DateParameter(default=datetime.datetime.today())
+    graph_url = luigi.Parameter(default="")
 
     def requires(self):
         test = not self.production
-        return CooccuranceTableNeo4jTask(test=test,
-                                         date=date)
+        return CooccurrenceTableNeo4jTask(test=test,
+                                          date=self.date,
+                                          graph_url=self.graph_url)
