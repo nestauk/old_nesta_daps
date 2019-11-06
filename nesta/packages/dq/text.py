c4a95589fcc088d82d5f73232b98df2d94ef33a9
dede95 <adotu95@gmail.com>
2019-11-06 15:53:24 +0000

    Updated text.py and test_text.py

--- nesta/packages/dq/text.py
+++ nesta/packages/dq/text.py
@@ -2,80 +2,47 @@ import pandas as pd
 import itertools
 from collections import Counter
 ##string length
-def string_length(input):#, column):
+def string_length(s, include_nan= False):
     '''string_length
     Calculates the character-length of strings.
 
     Args:
-        input (:obj:`iter` of :obj:`str`): A sequence of string objects.
-
+        s (:obj:`iter` of :obj:`str`): A sequence of string objects.
+        include_nan (:obj:`str`): Option to count NaN values
 
     Returns:
         flat_length_series (:obj:`pandas.core.series.Series`): A series onject of string lengths.
     '''
+    if include_nan == False:
+        series = pd.Series(s)
 
-    series = pd.Series(input)
+        flat_length_list = series[~pd.isnull(series)].str.len().values
+        flat_length_series = pd.Series(flat_length_list)
 
-    flat_length_list = series[~pd.isnull(series)].str.len().values
-    flat_length_series = pd.Series(flat_length_list)
+    elif include_nan == True:
+        series = pd.Series(s)
+        series.fillna('', inplace= True)
+        flat_length_list = series[~pd.isnull(series)].str.len().values
+        flat_length_series = pd.Series(flat_length_list)
 
     return flat_length_series
 
 
-def string_counter(input):
-    '''string_counter?
-
+def string_counter(s):
+    '''string_counter
+    Count of unique strings in dataset.
 
     Args:
-        input (:obj:`iter` of :obj:`list`): A sequence of string objects.
+        s (:obj:`iter` of :obj:`list`): A sequence of string objects.
 
     Returns:
         count_output (:obj:`pandas.core.series.Series`): A series object of strings and string freuqencies.
     '''
 
-    series = pd.Series(input)
+    series = pd.Series(s)
     count = Counter(series)
-    count_output = pd.Series(count)
-    return count_output
-
-##keyword
-# def delimiter_splitter(n, delimiter):
-#     '''delimiter_splitter
-#     Args:
-#         n (:obj:`str`): A string.
-#         delimiter (:obj:`str`): A string of a delimiter (punctuation, white space, etc).
-#
-#     Returns:
-#         output (:obj:`list`): A list of string tokens.
-#
-#     '''
-#
-#     if delimiter in n:
-#         if delimiter+' ' in n:
-#             output = n.split(delimiter+' ')
-#             return output
-#         else:
-#             output = n.split(delimiter)
-#             return output
-#     else:
-#         output = [n]
-#         return output
-#
-# def split_and_replace(input, delimiter):
-#
-#     '''split_and_replace
-#
-#     Args:
-#         input (:obj:`iter` of :obj:`list`): A sequence of string objects.
-#         delimiter (:obj:`str`): A string of a delimiter (punctuation, white space, etc).
-#
-#     Returns:
-#         flat_output_clean (:obj:`list`): A list of string tokens.
-#     '''
-#
-#     series = pd.Series(input)
-#     output = input.dropna().apply(lambda x: delimiter_splitter(x,delimiter) if type(x) == str else None).values.tolist()
-#     flat_output = [val for sublist in output for val in sublist if val != '']
-#     flat_output_clean = [val.lower().replace(".", "") for val in flat_output if '?' not in val]
-#
-#     return flat_output_clean
+    count = (series.value_counts()
+                     .reset_index()
+                     .rename(columns={'index': 'Text', series.name: 'Frequency'}))
+    # count_output = pd.Series(count)
+    return count#_output
