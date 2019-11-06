8618a1ed95913d05c459b713e20830bfeab1182e
dede95 <adotu95@gmail.com>
2019-11-06 15:51:34 +0000

    Updated text.py and test_text.py

--- nesta/packages/dq/tests/test_text.py
+++ nesta/packages/dq/tests/test_text.py
@@ -7,7 +7,7 @@ import pytest
 
 # from nesta.packages.dq.text import (string_length, string_counter)
 import sys
-sys.path.insert(0, '/mnt/c/Users/aotubusen/Google Drive/Documents/DS Projects/nesta/nesta/packages/dq')
+sys.path.insert(0, '/mnt/c/Users/aotubusen/Documents/DS Projects/nesta_dq/nesta/nesta/packages/dq/')
 from text import (string_length, string_counter)
 
 @pytest.fixture
@@ -26,9 +26,18 @@ class TestStringDataQuality():
         assert_array_equal(result, expected_data)
 
     def test_string_counter(self, data_string):
-        result =  dict(string_counter(data_string))
+        output =  string_counter(data_string)
+        result = dict(zip(output['Text'], output[0]))
 
 
-        expected_data = {'a': 1, 'ab': 1, 'abc': 1, 'ac': 1, 'b': 1,'bc': 1, 'c': 1, 'cde': 1, 'de': 2, 'efg': 1}
-
-        assert all(v == result[k] for k,v in expected_data.items())
+        expected_data = {'de': 2,
+                         'ab': 1,
+                         'ac': 1,
+                         'c': 1,
+                         'a': 1,
+                         'abc': 1,
+                         'bc': 1,
+                         'cde': 1,
+                         'efg': 1,
+                         'b': 1}
+        assert result == expected_data
