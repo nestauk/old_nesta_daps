0da90430e9d8ecdbd8cd99ff2dca547762928c71
Joel Klinger <joel.klinger@nesta.org.uk>
2020-05-29 13:38:12 +0000

    installed git-crypt

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 chmod +x githooks/pre-commit
-bash githooks/pre-commit && ln -s githooks/pre-commit .git/hooks/pre-commit
+bash githooks/pre-commit && ln -s githooks/pre-commit .git/hooks/pre-commit || exit 1
 echo "You should now run 'git-crypt unlock /path/to/key' to access config files"
