845c1075be9b52510756ab1d9258ce212a141098
Joel Klinger <joel.klinger@nesta.org.uk>
2020-07-22 11:23:26 +0000

    refactored for readability

--- nesta/core/batchables/general/arxiv/run.py
+++ nesta/core/batchables/general/arxiv/run.py
@@ -4,7 +4,7 @@ run.py (general.arxiv)
 
 Transfer pre-collected arXiv data from MySQL
 to Elasticsearch, whilst labelling arXiv articles
-as being EU or not. This differs slightly from 
+as being EU or not. This differs slightly from
 the `arXlive <http://arxlive.org>`_ pipeline, by reflecting the
 EURITO project more specificially, and allowing more
 in depth analysis of MAG fields of study.
@@ -20,7 +20,6 @@ from nuts_finder import NutsFinder
 from nesta.core.luigihacks.elasticsearchplus import ElasticsearchPlus
 from nesta.core.luigihacks.luigi_logging import set_log_level
 from nesta.core.orms.orm_utils import db_session, get_mysql_engine
-from nesta.core.orms.orm_utils import load_json_from_pathstub
 from nesta.core.orms.orm_utils import object_to_dict
 from nesta.core.orms.arxiv_orm import Article as Art
 from nesta.core.orms.grid_orm import Institute as Inst
@@ -29,6 +28,95 @@ from nesta.packages.mag.fos_lookup import build_fos_lookup
 from nesta.packages.mag.fos_lookup import make_fos_tree
 from nesta.packages.geo_utils.lookup import get_country_region_lookup
 from nesta.packages.geo_utils.lookup import get_eu_countries
+
+
+def generate_grid_lookup(engine):
+    class Object(object):
+        pass
+    country_lookup = get_country_region_lookup()  #<<<ADD CACHE
+    grid_lookup = defaultdict(Object)
+    with db_session(engine) as session:
+        for inst in session.query(Inst).all():
+            grid_lookup[inst.id].country = inst.country_code
+            grid_lookup[inst.id].region = country_lookup.get(inst.country_code)
+            grid_lookup[inst.id].name = inst.name
+            grid_lookup[inst.id].latlon = (inst.latitude, inst.longitude)
+    return grid_lookup
+
+
+def flatten_fos(row):
+    return [f for fields in row['fields_of_study']['nodes']
+            for f in fields if f != []]
+
+def flatten_categories(categories):
+    return [cat['description'] for cat in categories]
+
+
+def calculate_nuts_regions(row, institutes, nuts_finder):
+    # Add NUTS regions
+    for inst in institutes:
+        lat, lon = inst.latlon
+        if lat is None or lon is None:
+            continue
+        nuts = nuts_finder.find(lat=lat, lon=lon)
+        for i in range(0, 4):
+            name = f'nuts_{i}'
+            if name not in row:
+                row[name] = set()
+            for nut in nuts:
+                if nut['LEVL_CODE'] != i:
+                    continue
+                row[name].add(nut['NUTS_ID'])
+    for i in range(0, 4):
+        name = f'nuts_{i}'
+        if name in row:
+            row[name] = list(row[name])
+    return row
+
+def generate_authors_and_institutes(mag_authors, good_lookup):
+    authors, institutes = None, []
+    if mag_authors is not None:
+        if all('author_order' in a for a in mag_authors):
+            mag_authors = sorted(mag_authors, key=lambda a: a['author_order'])
+        gids = [author.get('affiliation_grid_id') for author in mag_authors]
+        authors = [author['author_name'].title() for author in mag_authors]
+        institutes = [good_lookup[g].name.title() for g in gids if g in good_lookup]
+    if institutes == []:
+        institutes = [inst.name.title() for inst in good_lookup.values()]
+    return authors, institutes
+
+
+def reformat_row(row, grid_lookup, nuts_finder, inst_matching_threshold=0.9):
+
+    # Create intermediate fields
+    mag_authors = row.pop('mag_authors')
+    categories = row.pop('categories')
+    institutes = row.pop('institutes')
+    good_institutes = [inst['institute_id'] for inst in institutes
+                       if inst['matching_score'] > inst_matching_threshold]
+    good_lookup = {_id: inst for _id, inst in grid_lookup.items() if _id in good_institutes}
+    good_institutes = [inst for _id, inst in grid_lookup.items() if _id in good_institutes]
+    all_countries = set(inst.country for inst in grid_lookup.values())
+    countries = set(inst.country for inst in good_institutes if inst.country is not None)
+    regions = set(inst.region for inst in good_institutes if inst.region is not None)
+    has_mn = any(is_multinational(inst.name, all_countries)
+                 for inst in good_institutes)
+    eu_countries = get_eu_countries()  ##<<ADD CACHE
+    authors, institutes = generate_authors_and_institutes(mag_authors, good_lookup)
+
+    # Input final fields
+    row['year'] = row['created'].year if row['created'] is not None else None
+    row['categories'] = [cat['description'] for cat in categories]
+    row['fields_of_study'] = make_fos_tree(row['fields_of_study'], fos_lookup)
+    row['_fields_of_study'] = flatten_fos(row)
+    row['countries'] = list(countries)
+    row['regions'] = [region for country, region in regions]
+    row['is_eu'] = any(country in eu_countries for country in countries)
+    row['has_multinational'] = has_mn
+    row = calculate_nuts_regions(row, institutes, nuts_finder)
+    row['institutes'] = institutes
+    row['authors'] = authors
+    return row
 
 
 def run():
@@ -51,7 +139,7 @@ def run():
     logging.info('Building FOS lookup')
     fos_lookup = build_fos_lookup(engine, max_lvl=6)
     nf = NutsFinder()
-    
+
     # es setup
     logging.info('Connecting to ES')
     strans_kwargs = {'filename': 'arxiv.json', 'ignore': ['id']}
@@ -77,118 +165,21 @@ def run():
     logging.info(f"{len(art_ids)} article IDs "
                  "retrieved from s3")
 
-    # Get all grid countries
-    # and country: continent lookup
+    # Generate lookup tables
     logging.info('Doing country lookup')
-    country_lookup = get_country_region_lookup()            
-    eu_countries = get_eu_countries()
-    with db_session(engine) as session:
-        grid_regions = {obj.id: country_lookup[obj.country_code]
-                       for obj in session.query(Inst).all()
-                       if obj.country_code is not None}
-        grid_countries = {obj.id: obj.country_code
-                          for obj in session.query(Inst).all()
-                          if obj.country_code is not None}
-        grid_institutes = {obj.id: obj.name
-                           for obj in session.query(Inst).all()}
-        grid_latlon = {obj.id: (obj.latitude, obj.longitude)
-                       for obj in session.query(Inst).all()}
+    # Generate GRID institute look-up tables
+    grid_lookup = generate_grid_lookup(engine)
 
-    #
+    # Iterate over articles
     logging.info('Processing rows')
     with db_session(engine) as session:
         for count, obj in enumerate((session.query(Art)
                                      .filter(Art.id.in_(art_ids))
                                      .all())):
             row = object_to_dict(obj)
-            # Extract year from date
-            if row['created'] is not None:
-                row['year'] = row['created'].year
-
-            # Normalise citation count for searchkit
-            if row['citation_count'] is None:
-                row['citation_count'] = 0
-
-            # Extract field of study
-            row['fields_of_study'] = make_fos_tree(row['fields_of_study'],
-                                                   fos_lookup)
-            row['_fields_of_study'] = [f for fields in
-                                       row['fields_of_study']['nodes']
-                                       for f in fields if f != []]
-
-            # Format hierarchical fields as expected by searchkit
-            row['categories'] = [cat['description']
-                                 for cat in row.pop('categories')]
-            institutes = row.pop('institutes')
-            good_institutes = [i['institute_id']
-                               for i in institutes
-                               if i['matching_score'] > 0.9]
-
-            # Add NUTS regions
-            for inst_id in good_institutes:
-                if inst_id not in grid_latlon:
-                    continue
-                lat, lon = grid_latlon[inst_id]
-                if lat is None or lon is None:
-                    continue
-                nuts = nf.find(lat=lat, lon=lon)
-                for i in range(0, 4):
-                    name = f'nuts_{i}'
-                    if name not in row:
-                        row[name] = set()
-                    for nut in nuts:
-                        if nut['LEVL_CODE'] != i:
-                            continue
-                        row[name].add(nut['NUTS_ID'])
-            for i in range(0, 4):
-                name = f'nuts_{i}'
-                if name in row:
-                    row[name] = list(row[name])
-
-            # Add other geographies
-            countries = set(grid_countries[inst_id]
-                            for inst_id in good_institutes
-                            if inst_id in grid_countries)
-            regions = set(grid_regions[inst_id]
-                          for inst_id in good_institutes
-                          if inst_id in grid_countries)
-            row['countries'] = list(countries) #[c for c, r in countries]
-            row['regions'] = [r for c, r in regions]
-            row['is_eu'] = any(c in eu_countries
-                               for c in countries)
-
-            # Pull out international institute info
-            has_mn = any(is_multinational(inst,
-                                          grid_countries.values())
-                         for inst in good_institutes)
-            row['has_multinational'] = has_mn
-
-            # Generate author & institute properties
-            mag_authors = row.pop('mag_authors')
-            if mag_authors is None:
-                row['authors'] = None
-                row['institutes'] = None 
-            else:
-                if all('author_order' in a for a in mag_authors):
-                    mag_authors = sorted(mag_authors,
-                                         key=lambda a:
-                                         a['author_order'])
-                row['authors'] = [author['author_name'].title()
-                                  for author in mag_authors]
-                gids = [author['affiliation_grid_id']
-                        for author in mag_authors
-                        if 'affiliation_grid_id' in author]
-                row['institutes'] = [grid_institutes[g].title()
-                                     for g in gids
-                                     if g in grid_institutes
-                                     and g in good_institutes]
-            if row['institutes'] in (None, []):
-                row['institutes'] = [grid_institutes[g].title()
-                                     for g in good_institutes]
-
-            uid = row.pop('id')
+            row = reformat_row(row, grid_lookup, nf)
             _row = es.index(index=es_index, doc_type=es_type,
-                            id=uid, body=row)
+                            id=row.pop('id'), body=row)
             if not count % 1000:
                 logging.info(f"{count} rows loaded to "
                              "elasticsearch")
