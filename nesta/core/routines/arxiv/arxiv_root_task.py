27578ccb7dba524ed8958d3328f8097496575688
Joel Klinger <joel.klinger@nesta.org.uk>
2020-07-27 10:19:07 +0000

    added logging to debug better

--- nesta/core/routines/arxiv/arxiv_root_task.py
+++ nesta/core/routines/arxiv/arxiv_root_task.py
@@ -41,6 +41,8 @@ class RootTask(luigi.WrapperTask):
     def requires(self):
         '''Collects the database configurations
         and executes the central task.'''
+        logging.getLogger().setLevel(logging.INFO)
+
         _routine_id = "{}-{}".format(self.date, self.production)
         grid_task_kwargs = {
             '_routine_id':_routine_id,
