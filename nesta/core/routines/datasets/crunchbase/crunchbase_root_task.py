ec41fac600e6735ffebba46405ab01bb06a295cb
Joel Klinger <joel.klinger@nesta.org.uk>
2020-07-27 16:02:03 +0000

    syntax error in orm

--- nesta/core/routines/datasets/crunchbase/crunchbase_root_task.py
+++ nesta/core/routines/datasets/crunchbase/crunchbase_root_task.py
@@ -30,27 +30,29 @@ class RootTask(luigi.WrapperTask):
     insert_batch_size = luigi.IntParameter(default=500)
     db_config_path = luigi.Parameter(default=f3p("mysqldb.config"))
     db_config_env = luigi.Parameter(default="MYSQLDB")
-    yield ParentIdCollectTask(date=self.date,
-                              _routine_id=self._routine_id,
-                              test=self.test,
-                              insert_batch_size=self.insert_batch_size,
-                              db_config_path=self.db_config_path,
-                              db_config_env=self.db_config_env)
-
-    yield OrgGeocodeTask(date=self.date,
-                         _routine_id=self._routine_id,
-                         test=self.test,
-                         db_config_env=self.db_config_env,
-                         city_col=Organization.city,
-                         country_col=Organization.country,
-                         location_key_col=Organization.location_id,
-                         insert_batch_size=self.insert_batch_size,
-                         env_files=[f3p("nesta/nesta/"),
-                                    f3p("config/mysqldb.config")],
-                         job_def="py36_amzn1_image",
-                         job_name=f"CrunchBaseOrgGeocodeTask-{self._routine_id}",
-                         job_queue="HighPriority",
-                         region_name="eu-west-2",
-                         poll_time=10,
-                         memory=4096,
-                         max_live_jobs=2)
+    
+    def requires(self):
+        yield ParentIdCollectTask(date=self.date,
+                                  _routine_id=self._routine_id,
+                                  test=self.test,
+                                  insert_batch_size=self.insert_batch_size,
+                                  db_config_path=self.db_config_path,
+                                  db_config_env=self.db_config_env)
+        
+        yield OrgGeocodeTask(date=self.date,
+                             _routine_id=self._routine_id,
+                             test=self.test,
+                             db_config_env=self.db_config_env,
+                             city_col=Organization.city,
+                             country_col=Organization.country,
+                             location_key_col=Organization.location_id,
+                             insert_batch_size=self.insert_batch_size,
+                             env_files=[f3p("nesta/nesta/"),
+                                        f3p("config/mysqldb.config")],
+                             job_def="py36_amzn1_image",
+                             job_name=f"CrunchBaseOrgGeocodeTask-{self._routine_id}",
+                             job_queue="HighPriority",
+                             region_name="eu-west-2",
+                             poll_time=10,
+                             memory=4096,
+                             max_live_jobs=2)
