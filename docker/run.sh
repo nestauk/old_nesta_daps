39e0b9a1d257fa0ac552e87e81c1bdfaa788691d
Joel Klinger <joel.klinger@nesta.org.uk>
2020-07-29 09:54:41 +0000

    unstaged run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # uncomment to enable the central scheduler (maybe useful for the graphical interface)
-#luigid &
+# luigid &
 
 # pass any arguments straight on to luigi
 luigi --logging-conf-file /app/docker/luigi_logging_docker.cfg --local-scheduler "$@"
