175efef6ada3a86e5ce1eef1c7820c6e93083e71
Joel Klinger <joel.klinger@nesta.org.uk>
2020-08-04 13:25:37 +0000

    tidied run file

--- nesta/core/batchables/general/companies/sql2es/run.py
+++ nesta/core/batchables/general/companies/sql2es/run.py
@@ -40,7 +40,7 @@ def run():
                            entity_type=entity_type,
                            strans_kwargs={'filename': 'companies.json'})
 
-    # collect file
+    # Collect input file
     s3 = boto3.resource('s3')
     obj = s3.Object(bucket, batch_file)
     org_ids = json.loads(obj.get()['Body']._raw_stream.read())
@@ -62,29 +62,4 @@ if __name__ == "__main__":
     logging.basicConfig(handlers=[log_stream_handler, ],
                         level=logging.INFO,
                         format="%(asctime)s:%(levelname)s:%(message)s")
-
-    if 'BATCHPAR_outinfo' not in os.environ:
-        from nesta.core.orms.orm_utils import setup_es
-        es, es_config = setup_es(production=False, endpoint='health-scanner',
-                                 dataset='companies',
-                                 drop_and_recreate=True)
-
-        environ = {"AWSBATCHTEST": "",
-                   'BATCHPAR_batch_file': 'crunchbase_to_es-15597291977144725.json', 
-                   'BATCHPAR_config': ('/home/ec2-user/nesta/nesta/'
-                                       'core/config/mysqldb.config'),
-                   'BATCHPAR_db_name': 'production', 
-                   'BATCHPAR_bucket': 'nesta-production-intermediate', 
-                   'BATCHPAR_done': "False", 
-                   'BATCHPAR_outinfo': ('https://search-health-scanner-'
-                               '5cs7g52446h7qscocqmiky5dn4.'
-                               'eu-west-2.es.amazonaws.com'), 
-                   'BATCHPAR_out_port': '443', 
-                   'BATCHPAR_out_index': 'companies_v1', 
-                   'BATCHPAR_out_type': '_doc', 
-                   'BATCHPAR_aws_auth_region': 'eu-west-2', 
-                   'BATCHPAR_entity_type': 'company', 
-                   'BATCHPAR_test': "False"}
-        for k, v in environ.items():
-            os.environ[k] = v
     run()
