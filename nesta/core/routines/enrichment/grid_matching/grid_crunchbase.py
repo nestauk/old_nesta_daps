08fc6a7c10ec5adb20203d4fe9779cca4f3b6cbb
Joel Klinger <joel.klinger@nesta.org.uk>
2020-08-20 13:41:02 +0000

    commented grid matcher

--- nesta/core/routines/enrichment/grid_matching/grid_crunchbase.py
+++ nesta/core/routines/enrichment/grid_matching/grid_crunchbase.py
@@ -1,25 +1,31 @@
 from nesta.packages.grid.grid_matching import process_name
 from nesta.packages.grid.grid_matching import GridMatcher
 
-class GridCBMatchingTask(luigi.Task):
-    
-    def run(self):
-        engine = get_mysql_engine("MYSQLDB", "mysqldb", "production") 
-        name_fields = ['alias1', 'alias2', 'alias3', 'legal_name', 'name']
-        cb_fields = ['id', 'country_code', 'parent_id'] + name_fields
-        chunks = pd.read_sql_table('crunchbase_organizations', engine, columns=cb_fields , chunksize=10000)
-        cb_df = pd.concat(chunks)        
-        cb_df['names'] = cb_df.apply(lambda row: {process_name(row[k]) for k in name_fields 
-                                                  if k in row and row[k] is not None}, axis=1)
-        cb_df["iso2_code"] = cb_df.country_code.apply(lambda iso3: alpha3_to_alpha2[iso3])
-        cb_data = {row['id']: {"names":row["names"], "iso2_code": row["iso2_code"]}
-                   for _, row in cb_df.iterrows()}
-        del cb_df
-        del chunks
+def _process(row, name_fields):
+    return {process_name(row[k]) for k in name_fields
+            if k in row and row[k] is not None}
 
+def read_cb_data(db):
+    engine = get_mysql_engine("MYSQLDB", "mysqldb", db)
+    name_fields = ['alias1', 'alias2', 'alias3', 'legal_name', 'name']
+    cb_fields = ['id', 'country_code', 'parent_id'] + name_fields
+    chunks = pd.read_sql_table('crunchbase_organizations', engine,
+                               columns=cb_fields , chunksize=10000)
+    df = pd.concat(chunks)
+    df['names'] = df.apply(lambda r: _process(r, name_fields), axis=1)
+    df["iso2_code"] = df.country_code.apply(lambda i: alpha3_to_alpha2[i])
+    data = {row['id']: {"names":row["names"],
+                        "iso2_code": row["iso2_code"]}
+            for _, row in df.iterrows()}
+    return data
+
+
+class GridCBMatchingTask(luigi.Task):
+
+    def run(self):
+        cb_data = read_cb_data(db="production")
         # Takes about 25 mins
         matcher = GridMatcher()
         matches = matcher.generate_matches(cb_data)
-        
         # write to disk
         write_data(data=matches, Base=Base, etc=etc)
--- nesta/packages/grid/grid_matcher.py
+++ nesta/packages/grid/grid_matcher.py
@@ -50,6 +50,37 @@ def append_disputed_countries(grid_ctrys):
     return grid_ctrys.union(_disputed_ctrys)
 
 
+def generate_grid_lookups():
+    # Generate GRID ID-country lookup
+    engine = get_mysql_engine("MYSQLDB", "mysqldb", "production")
+    chunks = pd.read_sql_table('grid_institutes', engine,
+                               columns=["id", "city", "country_code"],
+                               chunksize=1000)
+    grid_df = pd.concat([df for df in chunks])
+    grid_ctry_lookup = {row['id']:
+                        alpha2_to_alpha3[row['country_code']]
+                        for _, row in grid_df.iterrows()}
+
+    # Generate reverse lookup
+    _lookup = grid_name_lookup(engine)
+    lookup = defaultdict(set)
+    name_id_lookup = defaultdict(set)
+    for name, ids in _lookup.items():
+        _name = process_name(name)
+        for _id in ids:
+            lookup[_id].add(_name)
+        name_id_lookup[_name] = name_id_lookup[_name].union(ids)
+    # don't want defdict behaviour after this point
+    name_id_lookup = {k: v for k, v in name_id_lookup.items()}
+
+    # Generate list of all names for the inst, including
+    # duplicates and aliases
+    grid_df['names'] = grid_df.apply(lambda row:
+                                     lookup[row['id']], axis=1)
+    all_grid_names = set().union(*grid_df.names)
+    return all_grid_names, name_id_lookup, grid_ctry_lookup
+
+
 def _evaluate_matches(match_scores, ctry_code,
                       name_id_lookup, grid_ctry_lookup,
                       score_threshold=1,
@@ -138,12 +169,6 @@ class MatchEvaluator:
     based on a series of criteria.
 
     Args:
-        match_scores (dict): Precalculated match terms and scores
-        ctry_code (str): Country code of the organisation we're
-                         trying to match.
-        name_id_lookup (dict): Lookup of GRID names to IDs
-                               (including aliases)
-        grid_ctry_lookup (dict): Lookup of GRID ID to country code
         score_threshold (float): Only consider matches with at
                                  least this score.
         multinat_threshold (int): Consider an organisation to be
@@ -161,15 +186,21 @@ class MatchEvaluator:
                                    accepted, regardless of the country
                                    match.
     """
-    def __init__(self, name_id_lookup, grid_ctry_lookup,
-                 score_threshold=1, multinat_threshold=3,
+    def __init__(self, score_threshold=1, multinat_threshold=3,
                  multimatch_threshold=2, long_name_threshold=4):
         self.s_threshold = score_threshold
         self.mn_threshold = multinat_threshold
         self.mm_threshold = multimatch_threshold
         self.ln_threshold = long_name_threshold
         self.name_id_lookup = name_id_lookup
+
+        # Generate GRID lookup tables
+        lookups = generate_grid_lookups()
+        all_grid_names, name_id_lookup, grid_ctry_lookup = lookups
+        self.all_grid_names = all_grid_names
+        self.name_id_lookup = name_id_lookup
         self.grid_ctry_lookup = grid_ctry_lookup
+
 
     def evaluate_matches(self, ctry_code, match_scores):
         """Shallow wrapper around :obj:`_evaluate_matches`,
@@ -185,47 +216,101 @@ class MatchEvaluator:
 
 
     def generate_matches(self, data):
+        """Find exact and near matches, then filter matches
+        based on the matching criteria described in
+        :obj:`_evaluate_matches`.
+
+        Args:
+            data (list of dict): List of rows, of the format
+                                 {'id': 123,
+                                  'names':[('apple','inc'),('apple',)],
+                                  'iso2_code': 'US'}
+        Returns:
+            matches (dict): Mapping of ID from the input data to matching
+                            GRID IDs
+        """
         matches, remaining_names = find_exact_matches(data)
-        jaccard_matches = fast_nested_jaccard(remaining_names, 
-                                              self.all_grid_names)
-        inexact_matches = find_inexact_matches(data, matches, 
+        exact_matches = set(matches.keys())
+        jaccard_matches = fast_nested_jaccard(remaining_names)
+        inexact_matches = find_inexact_matches(data, exact_matches,
                                                jaccard_matches)
         matches.update(inexact_matches)
         return matches
 
 
-def find_exact_matches(data):
-    all_grid_names = set().union(*self.grid_df.names)
-    all_comparison_names = set().union(row['names'] for row in data.values())
-    exact_matches = all_comparison_names.intersection(self.all_grid_names)
+    def find_exact_matches(self, data):
+        """Find exact matches to GRID organisations, based on
+        the organisation name.
 
-    matches = {}
-    matched_names = set()
-    for id, row in data.items():
-        matched_names = row["names"].intersection(exact_matches)
-        gids, best_score = matcher.find_matches(iso2_code=row['iso2_code'],
-                                                match_scores={m: 1 for m in matched_names})
-        if best_score is not None:
-            matches[id] = {'grid_ids': gids, 'score': best_score}
-            matched_names = matched_names.union(row["names"])
-    remaining_names = all_comparison_names - matched_names
-    return matches, remaining_names
+        Args:
+            data (list of dict): List of rows, of the format
+                                 {'id': 123,
+                                  'names':[('apple','inc'),('apple',)],
+                                  'iso2_code': 'US'}
+        Returns:
+            matches (dict): Mapping of ID from the input data to matching
+                            GRID IDs
+        """
+        all_names = set().union(row['names'] for row in data.values())
+        exact_matches = all_names.intersection(self.all_grid_names)
+        matches = {}
+        matched_names = set()
+        for id, row in data.items():
+            # Evaluate the matches for this row
+            matched_names = row["names"].intersection(exact_matches)
+            scores = match_scores={m: 1 for m in matched_names})
+            iso2 = row['iso2_code']
+            gids, best_score = self.evaluate_matches(iso2_code=iso2,
+                                                     match_scores=scores)
+            # best_score is only None if the match was rejected
+            if best_score is not None:
+                matches[id] = {'grid_ids': gids, 'score': best_score}
+                matched_names = matched_names.union(row["names"])
+        remaining_names = all_names - matched_names
+        return matches, remaining_names
 
-def find_inexact_matches(data, exact_matches, jaccard_matches):
-    matches = {}
-    for id, row in data.items():
-        if id in exact_matches:
-            continue
-        _score, _gids = 0, []
-        for name in row['names']:
-            if name not in jaccard_matches:
+    def find_inexact_matches(self, data, exact_matches, jaccard_matches):
+        """Find fuzzy matches to GRID organisations, based on
+        the organisation name.
+
+        Args:
+            data (list of dict): List of rows, of the format
+                                 {'id': 123,
+                                  'names':[('apple','inc'),('apple',)],
+                                  'iso2_code': 'US'}
+            exact_matches (set): Set of IDs already matched, so not to 
+                                 duplicate work.
+            jaccard_matches (dict of dict): Lookup table of nested jaccard
+                                            scores for fuzzy matches
+                                            between names in the data and
+                                            GRID.
+        Returns:
+            matches (dict): Mapping of ID from the input data to matching
+                            GRID IDs
+        """
+        matches = {}
+        for id, row in data.items():
+            # Don't duplicate re-evaluating exact matches
+            if id in exact_matches:
                 continue
-            match_scores = jaccard_matches[name]
-            gids, best_score = matcher.find_matches(iso2_code=row['country_code'],
-                                                    match_scores=match_scores)
-            if best_score is not None and best_score > _score:
-                _score = best_score
-                _gids = gids
-        if _score > 0:
-            matches[id] = {'grid_ids': gids, 'score': best_score}
-    return matches
+            # Find the best fuzzy match across all aliases
+            _score, _gids = 0, []
+            for name in row['names']:
+                # See if a fuzzy match has been found at all for this
+                if name not in jaccard_matches:
+                    continue
+                # Evaluate this match
+                scores = jaccard_matches[name]
+                iso2 = row['country_code']
+                gids, best = matcher.find_matches(iso2_code=iso2,
+                                                  match_scores=scores)
+                # If the match has been rejected, or it is worse than
+                # previous matches
+                if (best is None) or (best <= _score):
+                    continue
+                # Otherwise, assign the score
+                _score, _gids = best, gids
+            # If a good match was found, assign it
+            if _score > 0:
+                matches[id] = {'grid_ids': _gids, 'score': _score}
+        return matches
