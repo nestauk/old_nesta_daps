4254406fbe17eb45669791bae2fb727e907e7717
Joel Klinger <joel.klinger@nesta.org.uk>
2020-11-10 12:38:54 +0000

    stashing changes

--- nesta/core/batchables/general/nih/curate/run.py
+++ nesta/core/batchables/general/nih/curate/run.py
@@ -30,21 +30,60 @@ from nesta.core.orms.orm_utils import db_session, get_mysql_engine
 from nesta.core.orms.orm_utils import load_json_from_pathstub, insert_data
 
 from nesta.core.orms.orm_utils import object_to_dict
-from itertools import groupby 
-from operator import attrgetter 
+from itertools import groupby
+from operator import attrgetter
 
 
 # Input ORMs:
-from nesta.core.orms.nih_orm import Projects
-from nesta.core.orms.crunchbase_orm import Abstracts
-from nesta.core.orms.crunchbase_orm import Patents
-from nesta.core.orms.crunchbase_orm import LinkTables
-from nesta.core.orms.crunchbase_orm import ClinicalStudies
-from nesta.core.orms.crunchbase_orm import TextDuplicate
+from nesta.core.orms.nih_orm import Projects, Abstracts
+from nesta.core.orms.nih_orm import TextDuplicate
 
 # Output ORM
 from nesta.core.orms.general_orm import NihProject, Base
 
+def group_projects_by_core_id(engine, core_ids, nrows=None):
+    if core_ids is not None:
+        filter_stmt = ((Projects.core_project_num != None) & \
+                       (Projects.core_project_num.in_(core_ids)))
+    else:
+        filter_stmt = (Projects.core_project_num == None)
+
+    with db_session(engine) as session:
+        q = (session.query(Projects).filter(filter_stmt)
+             .order_by(Projects.core_project_num))
+        results = q.limit(nrows).all()
+        groups = [[object_to_dict(obj) for obj in group] for _, group in
+                  groupby(results, attrgetter('core_project_num'))]
+    return groups
+
+
+def retrieve_duplicates(engine, appl_ids, duplicate_threshold=0.8): 
+    # Retrieve all duplicate projects, which will include an overlap
+    # with appl_ids if there are already projects in this group
+    filter_stmt = ((TextDuplicate.application_id_1.in_(appl_ids) | 
+                    TextDuplicate.application_id_2.in_(appl_ids)) &  
+                    TextDuplicate.weight >= duplicate_threshold) 
+    with db_session(engine) as session: 
+        dupes = session.query(TextDuplicate).filter(filter_stmt).all() 
+        dupes = [object_to_dict(obj) for obj in dupes]
+
+    # Subset the new IDs, so we don't recollect any known duplicates
+    # in this group
+    dupe_ids = set([d['application_id_1'] for d in dupes] +\ 
+                   [d['application_id_2'] for d in dupes])
+    dupe_ids = dupe_ids - appl_ids
+
+    # Collect the new duplicates
+    filter_stmt = Projects.application_id.in_(dupe_ids)
+    with db_session(engine) as session: 
+        new_projs = session.query(Projects).filter(filter_stmt)
+        new_projs = [object_to_dict(obj) for obj in dupe_projs]
+    return new_projs 
+
+
+
+def assign_duplicates(row):
+    """For this row"""
 
 def reformat_row(row):
     """Curate raw data for ingestion to MySQL.
@@ -95,27 +134,12 @@ def run():
     core_ids = json.loads(obj.get()['Body']._raw_stream.read())
     logging.info(f"{len(core_ids)} projects retrieved from s3")
 
-    # Get the projects
-    with db_session(engine) as session:        
-        q = (session.query(Projects)
-             .filter(Projects.core_project_num.in_(core_ids))
-             .order_by(Projects.core_project_num))
-        results = q.limit(nrows).all()
-        groups = [[object_to_dict(obj) for obj in group] for _, group in 
-                  groupby(results, attrgetter('core_project_num'))]
-    
-    # If doing the "null" group
-    if do_null:
-        q = (session.query(Projects)
-             .filter(Projects.core_project_num == None))
-        results = q.limit(nrows).all()
-        groups += [[object_to_dict(obj)] for obj in results]
-
-
-    data = []
-    for group in groups:
-        row = concat_group(group)
+    data = []    
+    for group in group_projects_by_core_id(engine, core_ids):
         row = assign_duplicates(group)
+        if len(row) == 0:
+            continue
+        row = concat_group(group)
         row = reformat_row(row)
         data.append(row)
 
